4dd7224561b80d483d84df72178bb8acdcc2baf5
cuisongliu <cuisongliu@qq.com>
2022-04-26 11:33:34 +0800

    fix nvidia

--- runtime/applications/gpu-operator-centos7/charts/gpu-operator.sh
+++ runtime/applications/gpu-operator-centos7/charts/gpu-operator.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ../opt/helm install --wait --generate-name \
   -n gpu-operator --create-namespace \
- charts/gpu-operator-v1.10.1.tgz \
+  gpu-operator-v1.10.1.tgz \
   --set toolkit.version=1.7.1-centos7 --set driver.enabled=false
---
+++ runtime/applications/gpu-operator-ubuntu/init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+os=linux
+arch=${2:-amd64}
+mkdir -p opt
+wget https://get.helm.sh/helm-v3.8.2-$os-$arch.tar.gz -O helm.tar.gz && tar -zxvf helm.tar.gz
+mv linux-$arch/helm /opt/
