0817692bbd7348ec53ddd7bebe83b6ce858feff6
cuisongliu <cuisongliu@qq.com>
2022-05-02 23:34:52 +0800

    fix helm

---
+++ runtime/applications/helm/init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+arch=${1:-amd64}
+mkdir -p opt
+wget https://get.helm.sh/helm-v3.8.2-linux-$arch.tar.gz
+tar -zxvf helm-v3.8.2-linux-$arch.tar.gz
+chmod a+x linux-$arch/helm
+mv linux-$arch/helm opt/
+echo "download helm success"
