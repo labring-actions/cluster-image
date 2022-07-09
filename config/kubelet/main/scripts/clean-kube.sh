552554eb2530f248f5c1eeb09c28b8737fb8dcd7
cuisongliu <cuisongliu@qq.com>
2022-07-09 18:16:11 +0800

    fix kubelet image

---
+++ config/kubelet/main/scripts/clean-kube.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Copyright © 2022 sealyun.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+source common.sh
+systemctl stop kubelet
+systemctl daemon-reload
+
+rm -f /usr/bin/conntrack
+rm -f /usr/bin/kubelet-pre-start.sh
+rm -f /usr/bin/kubelet-post-stop.sh
+rm -f /usr/bin/kubeadm
+rm -f /usr/bin/kubectl
+rm -f /usr/bin/kubelet
+
+rm -f /etc/sysctl.d/k8s.conf
+rm -f /etc/systemd/system/kubelet.service
+rm -rf /etc/systemd/system/kubelet.service.d
+rm -rf /var/lib/kubelet/
+logger "clean kube success"
