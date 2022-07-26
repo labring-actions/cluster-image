bbe1c9510dc261f2c2b01ef959da124ebfb9b3a2
cuisongliu <cuisongliu@qq.com>
2022-07-26 22:07:10 +0800

    fix sealos verion github for config

---
+++ .github/scripts/download.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+VERSION=
+
+wget https://github.com/labring/sealos/releases/download/v$sealos/sealos_${VERSION}_linux_amd64.tar.gz
+tar -zxvf sealos_${VERSION}_linux_amd64.tar.gz sealos
+chmod a+x sealos
+sudo mv sealos /usr/bin/
+wget https://github.com/labring/cluster-image/releases/download/depend/buildah.linux.amd64 --no-check-certificate -O buildah && chmod a+x buildah
+sudo mv buildah /usr/bin/
+wget https://github.com/lework/skopeo-binary/releases/download/v1.9.1/skopeo-linux-amd64 -O skopeo
+chmod a+x skopeo
+sudo mv skopeo /usr/bin/
