fbb720a7573c182bc24de9498c2ca7d5ae91d51a
cuisongliu <cuisongliu@qq.com>
2022-07-26 22:15:28 +0800

    fix sealos verion github

--- .github/scripts/download.sh
+++ .github/scripts/download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://github.com/labring/sealos/releases/download/v$sealos/sealos_$sealos_linux_amd64.tar.gz
-tar -zxvf sealos_${VERSION}_linux_amd64.tar.gz sealos
+wget https://github.com/labring/sealos/releases/download/v$sealos/sealos_${sealos}_linux_amd64.tar.gz
+tar -zxvf sealos_${sealos}_linux_amd64.tar.gz sealos
 chmod a+x sealos
 sudo mv sealos /usr/bin/
 wget https://github.com/labring/cluster-image/releases/download/depend/buildah.linux.amd64 --no-check-certificate -O buildah && chmod a+x buildah
