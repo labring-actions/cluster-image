6eff90e8cf0b3d2c67e124d40f7f3e560a9aee6c
cuisongliu <cuisongliu@qq.com>
2022-07-27 23:22:56 +0800

    fix sealos verion github

--- .github/scripts/versions.sh
+++ .github/scripts/versions.sh
@@ -8,4 +8,6 @@ filePath=$(pwd)/.github/versions/CHANGELOG-${versionGroup}.md
 
 VERSIONS=$(cat $filePath| grep "\[v${versionGroup}" | grep -v "alpha" | grep -v "beta"| grep -v "rc" | awk '{print $2}' | cut -d '[' -f  2 | cut -d ']' -f 1   | awk '{printf "{\"'version'\":\"%s\"},",$1}')
 
+echo "versions is :$VERSIONS"
+
 echo "::set-output name=version::{\"include\":[${VERSIONS%?}]}"
