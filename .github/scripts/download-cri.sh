bd552790a261e8262940aaa43c759b4b67022ca3
cuisongliu <cuisongliu@qq.com>
2022-07-28 00:11:05 +0800

    fix sealos verion github

--- .github/scripts/download-cri.sh
+++ .github/scripts/download-cri.sh
@@ -16,34 +16,36 @@ fi
 
 mkdir -p .download/containerd/${arch}
 wget https://github.com/containerd/containerd/releases/download/v${containerdVersion}/cri-containerd-cni-${containerdVersion}-linux-${arch}.tar.gz --no-check-certificate -O cri-containerd-cni-linux.tar.gz
+if [ $? != 0 ]; then
+   echo "====download and targz containerd failed!===="
+   exit 1
+fi
 tar -zxf  cri-containerd-cni-linux.tar.gz
 rm -rf etc opt && mkdir -p usr/bin
 cp -rf usr/local/bin/* usr/bin/
 cp -rf usr/local/sbin/* usr/bin/ && rm -rf usr/bin/critest && rm -rf cri-containerd-cni-linux.tar.gz usr/local
 tar -czf cri-containerd-linux.tar.gz usr && rm -rf usr
 mv cri-containerd-linux.tar.gz .download/containerd/${arch}/
-if [ $? != 0 ]; then
-   echo "====download and targz containerd failed!===="
-   exit 1
-fi
+
 
 mkdir -p .download/nerdctl/${arch}
 wget https://github.com/containerd/nerdctl/releases/download/v${nerdctlVersion}/nerdctl-${nerdctlVersion}-linux-${arch}.tar.gz -O  nerdctl.tar.gz
-tar xf nerdctl.tar.gz nerdctl
-mv nerdctl .download/nerdctl/${arch}/
-chmod a+x .download/nerdctl/${arch}/nerdctl
-rm -rf nerdctl.tar.gz
 if [ $? != 0 ]; then
    echo "====download and targz nerdctl failed!===="
    exit 1
 fi
+tar xf nerdctl.tar.gz nerdctl
+mv nerdctl .download/nerdctl/${arch}/
+chmod a+x .download/nerdctl/${arch}/nerdctl
+rm -rf nerdctl.tar.gz
+
 
 
 mkdir -p .download/lsof/${arch}
 wget https://github.com/labring/cluster-image/releases/download/depend/lsof-linux-${arch} --no-check-certificate -O .download/lsof/${arch}/lsof
-chmod a+x .download/lsof/${arch}/lsof
-
 if [ $? != 0 ]; then
    echo "====download lsof failed!===="
    exit 1
 fi
+chmod a+x .download/lsof/${arch}/lsof
+
--- .github/scripts/kube-containerd.sh
+++ .github/scripts/kube-containerd.sh
@@ -5,7 +5,7 @@ mkdir -p $buildDir/bin && mkdir -p $buildDir/opt && mkdir -p $buildDir/registry 
 cp -rf rootfs/* $buildDir/
 cp -rf containerd/* $buildDir/
 # library install
-mv /tmp/library/${arch}/library.tar.gz .
+mv .download/library/${arch}/library.tar.gz .
 if [ $? != 0 ]; then
    echo "====cp library failed!===="
    exit 1
@@ -18,50 +18,50 @@ ls -l $buildDir/cri/lib64/lib
 rm -rf library
 cd $buildDir/cri/lib64 && tar -czf containerd-lib.tar.gz lib && rm -rf lib && cd ../../../
 #kube install
-mv /tmp/kube/${arch}/* $buildDir/bin/
+mv .download/kube/${arch}/* $buildDir/bin/
 if [ $? != 0 ]; then
    echo "====cp kube failed!===="
    exit 1
 fi
 # registry install
-mv /tmp/registry/${arch}/registry.tar $buildDir/images/registry.tar
+mv .download/registry/${arch}/registry.tar $buildDir/images/registry.tar
 if [ $? != 0 ]; then
    echo "====cp registry failed!===="
    exit 1
 fi
 # cri install
-mv /tmp/containerd/${arch}/cri-containerd-linux.tar.gz $buildDir/cri/
+mv .download/containerd/${arch}/cri-containerd-linux.tar.gz $buildDir/cri/
 if [ $? != 0 ]; then
    echo "====cp cri failed!===="
    exit 1
 fi
 # nerdctl install
-mv /tmp/nerdctl/${arch}/nerdctl $buildDir/cri/
+mv .download/nerdctl/${arch}/nerdctl $buildDir/cri/
 if [ $? != 0 ]; then
    echo "====cp nerdctl failed!===="
    exit 1
 fi
 # shim install
-mv /tmp/shim/${arch}/image-cri-shim $buildDir/cri/
+mv .download/shim/${arch}/image-cri-shim $buildDir/cri/
 if [ $? != 0 ]; then
    echo "====cp shim failed!===="
    exit 1
 fi
 # sealctl
-mv /tmp/sealctl/${arch}/image-cri-shim $buildDir/opt/
+mv .download/sealctl/${arch}/image-cri-shim $buildDir/opt/
 if [ $? != 0 ]; then
    echo "====cp sealctl failed!===="
    exit 1
 fi
 # lsof
-mv /tmp/lsof/${arch}/lsof $buildDir/opt/
+mv .download/lsof/${arch}/lsof $buildDir/opt/
 if [ $? != 0 ]; then
    echo "====cp lsof failed!===="
    exit 1
 fi
 chmod a+x $buildDir/opt/*
 # images
-cp -rf  /tmp/kubeadm/*ImageList   $buildDir/images/shim/
+cp -rf  .download/kubeadm/*ImageList   $buildDir/images/shim/
 # replace
 pauseImage=$(cat ./$buildDir/images/shim/DefaultImageList  | grep k8s.gcr.io/pause)
 sed -i "s#__pause__#k8s.gcr.io/${pauseImage##k8s.gcr.io/}#g" ./$buildDir/etc/kubelet-flags.env
