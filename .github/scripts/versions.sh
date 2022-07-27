59f7324d8f070dd68cfaa57dc7ce60787c1aacff
cuisongliu <cuisongliu@qq.com>
2022-07-27 23:41:00 +0800

    fix sealos verion github

--- .github/scripts/versions.sh
+++ .github/scripts/versions.sh
@@ -6,8 +6,8 @@ echo "Resolving versions in $(pwd)"
 
 filePath=$(pwd)/.github/versions/CHANGELOG-${versionGroup}.md
 
-VERSIONS=$(cat $filePath| grep "\[v${versionGroup}" | grep -v "alpha" | grep -v "beta"| grep -v "rc" | awk '{print $2}' | cut -d '[' -f  2 | cut -d ']' -f 1   | awk '{printf "{\"'version'\":\"%s\"},",$1}')
+VERSIONS=$(cat $filePath| grep "\[v${versionGroup}" | grep -v "alpha" | grep -v "beta"| grep -v "rc" | awk '{print $2}' | cut -d '[' -f  2 | cut -d ']' -f 1   | awk '{printf "{\"'version'\":\"%s\",\"'arch'\":\"amd64\"},{\"'version'\":\"%s\",\"'arch'\":\"arm64\"},",$1,$1}'
 
 echo "versions is : {\"include\":[${VERSIONS%?}]}"
 
-echo "::set-output name=version::{\"include\":[${VERSIONS%?}]}"
+echo "::set-output name=matrix::{\"include\":[${VERSIONS%?}]}"
