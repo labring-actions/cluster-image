aeead72090d8d809e2e27c78b23c873363589f45
cuisongliu <cuisongliu@qq.com>
2022-07-27 18:13:50 +0800

    fix sealos verion github

--- .github/scripts/apps.sh
+++ .github/scripts/apps.sh
@@ -17,13 +17,8 @@ if [ $? != 0 ]; then
    exit 1
 fi
 
-cd ../ && rm -rf $buildDir
 sudo buildah login --username $username --password $password $registry
 sudo buildah push $prefix/$app:$version-$arch
-sudo buildah manifest create $prefix/$app:$version
-sudo buildah manifest add $prefix/$app:$version docker://$prefix/$app:$version-amd64
-sudo buildah manifest add $prefix/$app:$version docker://$prefix/$app:$version-arm64
-sudo buildah manifest push --all $prefix/$app:$version docker://$prefix/$app:$version
 if [ $? != 0 ]; then
    echo "====push app image failed!===="
    exit 1
