e845446737f8b2c46b91aa89810fff4a4cb295d6
cuisongliu <cuisongliu@qq.com>
2022-07-28 00:16:28 +0800

    fix sealos verion github

--- .github/scripts/kube-containerd.sh
+++ .github/scripts/kube-containerd.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 buildDir=.build-image
 # init dir
+downloadDIR=$(pwd)/.download
 mkdir -p $buildDir/bin && mkdir -p $buildDir/opt && mkdir -p $buildDir/registry && mkdir -p $buildDir/images/shim && mkdir -p $buildDir/cri/lib64
 cp -rf rootfs/* $buildDir/
 cp -rf containerd/* $buildDir/
 # library install
-mv .download/library/${arch}/library.tar.gz .
+mv ${downloadDIR}/library/${arch}/library.tar.gz .
 if [ $? != 0 ]; then
    echo "====cp library failed!===="
    exit 1
@@ -18,56 +19,57 @@ ls -l $buildDir/cri/lib64/lib
 rm -rf library
 cd $buildDir/cri/lib64 && tar -czf containerd-lib.tar.gz lib && rm -rf lib && cd ../../../
 #kube install
-mv .download/kube/${arch}/* $buildDir/bin/
+mv ${downloadDIR}/kube/${arch}/* $buildDir/bin/
 if [ $? != 0 ]; then
    echo "====cp kube failed!===="
    exit 1
 fi
 # registry install
-mv .download/registry/${arch}/registry.tar $buildDir/images/registry.tar
+mv ${downloadDIR}/registry/${arch}/registry.tar $buildDir/images/registry.tar
 if [ $? != 0 ]; then
    echo "====cp registry failed!===="
    exit 1
 fi
 # cri install
-mv .download/containerd/${arch}/cri-containerd-linux.tar.gz $buildDir/cri/
+mv ${downloadDIR}/containerd/${arch}/cri-containerd-linux.tar.gz $buildDir/cri/
 if [ $? != 0 ]; then
    echo "====cp cri failed!===="
    exit 1
 fi
 # nerdctl install
-mv .download/nerdctl/${arch}/nerdctl $buildDir/cri/
+mv ${downloadDIR}/nerdctl/${arch}/nerdctl $buildDir/cri/
 if [ $? != 0 ]; then
    echo "====cp nerdctl failed!===="
    exit 1
 fi
 # shim install
-mv .download/shim/${arch}/image-cri-shim $buildDir/cri/
+mv ${downloadDIR}/shim/${arch}/image-cri-shim $buildDir/cri/
 if [ $? != 0 ]; then
    echo "====cp shim failed!===="
    exit 1
 fi
 # sealctl
-mv .download/sealctl/${arch}/image-cri-shim $buildDir/opt/
+mv ${downloadDIR}/sealctl/${arch}/sealctl $buildDir/opt/
 if [ $? != 0 ]; then
    echo "====cp sealctl failed!===="
    exit 1
 fi
 # lsof
-mv .download/lsof/${arch}/lsof $buildDir/opt/
+mv ${downloadDIR}/lsof/${arch}/lsof $buildDir/opt/
 if [ $? != 0 ]; then
    echo "====cp lsof failed!===="
    exit 1
 fi
 chmod a+x $buildDir/opt/*
 # images
-cp -rf  .download/kubeadm/*ImageList   $buildDir/images/shim/
+cp -rf  ${downloadDIR}/kubeadm/*ImageList   $buildDir/images/shim/
 # replace
 pauseImage=$(cat ./$buildDir/images/shim/DefaultImageList  | grep k8s.gcr.io/pause)
 sed -i "s#__pause__#k8s.gcr.io/${pauseImage##k8s.gcr.io/}#g" ./$buildDir/etc/kubelet-flags.env
 sed -i "s#__pause__#sealos.hub:5000/${pauseImage##k8s.gcr.io/}#g" ./$buildDir/etc/config.toml
 cd $buildDir
 chmod  -R 0755  *
+cat Kubefile
 sudo sealos build -t $registry/$repo/kubernetes:v${kubeVersion}-${arch} --platform linux/${arch} -f Kubefile  .
 
 sudo buildah login --username $username --password $password $registry
