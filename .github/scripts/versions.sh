c8edf201bffc3300375c14d78790d2001002e5e5
cuisongliu <cuisongliu@qq.com>
2022-07-28 00:26:32 +0800

    fix sealos verion github

--- .github/scripts/versions.sh
+++ .github/scripts/versions.sh
@@ -5,8 +5,12 @@ echo "Resolving versions in $(pwd)"
 
 filePath=$(pwd)/.github/versions/CHANGELOG-${versionGroup}.md
 
-VERSIONS=$(cat $filePath| grep "\[v${versionGroup}" | grep -v "alpha" | grep -v "beta"| grep -v "rc" | awk '{print $2}' | cut -d '[' -f  2 | cut -d ']' -f 1 | cut -d 'v' -f 2  | awk '{printf "{\"'version'\":\"%s\",\"'arch'\":\"amd64\"},{\"'version'\":\"%s\",\"'arch'\":\"arm64\"},",$1,$1}')
+VERSIONS_ARCH=$(cat $filePath| grep "\[v${versionGroup}" | grep -v "alpha" | grep -v "beta"| grep -v "rc" | awk '{print $2}' | cut -d '[' -f  2 | cut -d ']' -f 1 | cut -d 'v' -f 2  | awk '{printf "{\"'version'\":\"%s\",\"'arch'\":\"amd64\"},{\"'version'\":\"%s\",\"'arch'\":\"arm64\"},",$1,$1}')
+VERSIONS=$(cat $filePath| grep "\[v${versionGroup}" | grep -v "alpha" | grep -v "beta"| grep -v "rc" | awk '{print $2}' | cut -d '[' -f  2 | cut -d ']' -f 1 | cut -d 'v' -f 2  |  awk '{printf "{\"'version'\":\"%s\"},",$1}')
 
+echo "versions arch is : {\"include\":[${VERSIONS_ARCH%?}]}"
 echo "versions is : {\"include\":[${VERSIONS%?}]}"
 
-echo "::set-output name=matrix::{\"include\":[${VERSIONS%?}]}"
+
+echo "::set-output name=matrix::{\"include\":[${VERSIONS_ARCH%?}]}"
+echo "::set-output name=matrix_version::{\"include\":[${VERSIONS_ARCH%?}]}"
