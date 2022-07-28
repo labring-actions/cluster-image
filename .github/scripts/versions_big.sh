d34ec18cc4c94f8a4684a00c93005d7b4132ea36
cuisongliu <cuisongliu@qq.com>
2022-07-28 23:45:38 +0800

    fix sealos verion github

--- .github/scripts/versions_big.sh
+++ .github/scripts/versions_big.sh
@@ -4,7 +4,7 @@
 echo "Resolving big versions in $(pwd)"
 
 
-VERSIONS={\"version\":\"1.24\"},
+VERSIONS={\"version\":\"1.24\"},{\"version\":\"1.23\"},{\"version\":\"1.22\"},{\"version\":\"1.21\"},{\"version\":\"1.20\"},{\"version\":\"1.19\"},{\"version\":\"1.18\"},{\"version\":\"1.17\"},{\"version\":\"1.16\"},{\"version\":\"1.15\"},
 
 echo "versions is : {\"include\":[${VERSIONS%?}]}"
 
