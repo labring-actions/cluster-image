bd297f26d7a2711f33e27c47805c2f4c0fb1ac5d
cuisongliu <cuisongliu@qq.com>
2022-07-29 00:56:24 +0800

    fix sealos verion github

--- .github/scripts/versions.sh
+++ .github/scripts/versions.sh
@@ -4,11 +4,12 @@
 echo "Resolving versions in $(pwd)"
 mkdir -p .versions
 rm -rf .versions/versions.txt
-for file in $(pwd)/.github/versions/CHANGELOG*
+for file in $(pwd)/.github/versions/$dir/CHANGELOG*
 do
   versions=$(echo $file | cut -d '-' -f 3 | cut -d '.' -f 1,2)
   cat $file| grep "\[v"| grep -v "github"  | grep -v "alpha" | grep -v "stable"| grep -v "there" | grep -v "beta"| grep -v "rc" | awk '{print $2}' | cut -d '[' -f  2 | cut -d ']' -f 1 | cut -d 'v' -f 2  |  awk '{printf "{\"'version'\":\"%s\"},",$1}' >> .versions/versions.txt
 done
+echo $i
 VERSIONS=$(cat .versions/versions.txt)
 echo "versions is : {\"include\":[${VERSIONS%?}]}"
 
