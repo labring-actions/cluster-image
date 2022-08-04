daf29def74c209f19912f372003ff6b99f06f929
jianchang <420727434@qq.com>
2022-08-04 17:42:21 +0800

    add deepflow (#76)

---
+++ applications/deepflow/v6.1.1/init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+arch=${1:-amd64}
+mkdir -p opt
+wget https://deepflow-ce.oss-cn-beijing.aliyuncs.com/bin/ctl/latest/linux/$arch/deepflow-ctl -o opt/deepflow-ctl
+chmod a+x opt/deepflow-ctl
+echo "download deepflow-ctl success"
