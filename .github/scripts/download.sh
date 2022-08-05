6c65645024563ed031465b7bac1c20c89facdb64
jianchang <jianchang@yunshan.net>
2022-08-05 18:02:25 +0800

    update helm chart (#80)

--- .github/scripts/download.sh
+++ .github/scripts/download.sh
@@ -7,6 +7,11 @@ if [ $? != 0 ]; then
 fi
 chmod a+x sealos
 sudo mv sealos /usr/bin/
+wget https://get.helm.sh/helm-v3.8.2-linux-amd64.tar.gz
+tar -zxvf helm-v3.8.2-linux-amd64.tar.gz
+chmod a+x linux-amd64/helm
+sudo mv linux-amd64/helm /usr/bin/
+rm -rf linux-amd64 helm-v3.8.2-linux-amd64.tar.gz
 wget https://github.com/labring/cluster-image/releases/download/depend/buildah.linux.amd64 --no-check-certificate -O buildah && chmod a+x buildah
 if [ $? != 0 ]; then
    echo "====download buildah failed!===="
