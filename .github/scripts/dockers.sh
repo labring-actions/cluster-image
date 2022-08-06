365fdea26b6ac1ceee8cac10e166d4a0a3a0c210
cuisongliu <cuisongliu@qq.com>
2022-08-06 23:13:16 +0800

    add docker configs

--- .github/scripts/dockers.sh
+++ .github/scripts/dockers.sh
@@ -11,15 +11,15 @@ if  [ -f Dockerfile ]; then
   filename=Dockerfile
 fi
 [ -f init.sh  ] && sh init.sh $arch
-sudo sealos rmi -f $prefix/$app:$version-$arch || true
-sudo sealos build -t $prefix/$app:$version-$arch --platform linux/$arch -f $filename  .
+sudo sealos rmi -f $prefix/docker-$app:$version-$arch || true
+sudo sealos build -t $prefix/docker-$app:$version-$arch --platform linux/$arch -f $filename  .
 if [ $? != 0 ]; then
    echo "====build docker image failed!===="
    exit 1
 fi
 
 sudo sealos login $registry -u $username -p $password
-sudo sealos push $prefix/$app:$version-$arch
+sudo sealos push $prefix/docker-$app:$version-$arch
 if [ $? != 0 ]; then
    echo "====push docker image failed!===="
    exit 1
