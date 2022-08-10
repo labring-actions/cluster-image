0e2f09a85641c13b32dd588e357fc6a3c8dced52
cuisongliu <cuisongliu@qq.com>
2022-08-10 10:10:10 +0800

    feature(main): fix autobuild-hosted.yml to amd64
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

---
+++ .github/scripts/manifest-amd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+prefix=$registry/$repo
+sudo buildah rmi $prefix/$app:$version ||true
+sudo buildah login --username $username --password $password $registry
+sudo buildah manifest create $prefix/$app:$version
+sudo buildah manifest add $prefix/$app:$version docker://$prefix/$app:$version-amd64
+sudo buildah manifest push --all $prefix/$app:$version docker://$prefix/$app:$version
+if [ $? != 0 ]; then
+   echo "====push  image manifest failed!===="
+   exit 1
+fi
+sudo buildah rmi $prefix/$app:$version ||true
