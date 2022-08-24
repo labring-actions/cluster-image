51eae7959b50099f39c34220e0001d571bed2667
muicoder <muicoder@gmail.com>
2022-08-24 13:37:40 +0800

    feature(main): get versions

--- .github/scripts/versions.sh
+++ .github/scripts/versions.sh
@@ -4,14 +4,13 @@
 echo "Resolving versions in $(pwd)"
 mkdir -p .versions
 rm -rf .versions/versions.txt
-for file in $(pwd)/.github/versions/${part}/CHANGELOG*
-do
-  wget -qO "$file" "https://github.com/kubernetes/kubernetes/raw/master/CHANGELOG/${file##*/}"
-  versions=$(echo $file | cut -d '-' -f 3 | cut -d '.' -f 1,2)
-  cat $file| grep "\[v"| grep -v "github"  | grep -v "alpha" | grep -v "stable"| grep -v "there" | grep -v "beta"| grep -v "rc" | awk '{print $2}' | cut -d '[' -f  2 | cut -d ']' -f 1 | cut -d 'v' -f 2  |  awk '{printf "{\"'version'\":\"%s\"},",$1}' >> .versions/versions.txt
+for file in $(pwd)/.github/versions/${part}/CHANGELOG*; do
+  wget -qO- "https://github.com/kubernetes/kubernetes/raw/master/CHANGELOG/${file##*/}" |
+    grep -E '^- \[v[0-9\.]+\]' | awk '{print $2}' | awk -F\[ '{print $2}' | awk -F\] '{print $1}' |
+    cut -dv -f 2 |
+    awk '{printf "{\"'version'\":\"%s\"},",$1}' >>.versions/versions.txt
 done
 VERSIONS=$(cat .versions/versions.txt)
 echo "versions is : {\"include\":[${VERSIONS%?}]}"
-
 
 echo "::set-output name=matrix::{\"include\":[${VERSIONS%?}]}"
