1ae27a9840f4a5fd9977dd9ce9853737300954ab
muicoder <muicoder@gmail.com>
2022-08-29 18:07:18 +0800

    feature(main): update manifest.sh

--- .github/scripts/manifest.sh
+++ .github/scripts/manifest.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
-prefix=$registry/$repo
+
+set -e
 
 readonly CRI_TYPE=${criType}
-readonly version=${version?}
+
+readonly IMAGE_HUB_REGISTRY=${registry?}
+readonly IMAGE_HUB_REPO=${repo?}
+readonly IMAGE_HUB_USERNAME=${username?}
+readonly IMAGE_HUB_PASSWORD=${password?}
+
+readonly IMAGE_TAG=${version?}
 
 case $CRI_TYPE in
 containerd)
-  app=kubernetes
+  IMAGE_KUBE=kubernetes
   ;;
 docker)
-  app=kubernetes-docker
+  IMAGE_KUBE=kubernetes-docker
   ;;
 esac
 
-sudo buildah rmi $prefix/$app:$version ||true
-sudo buildah login --username $username --password $password $registry
-sudo buildah manifest create $prefix/$app:$version
-sudo buildah manifest add $prefix/$app:$version docker://$prefix/$app:$version-amd64
-sudo buildah manifest add $prefix/$app:$version docker://$prefix/$app:$version-arm64
-sudo buildah manifest push --all $prefix/$app:$version docker://$prefix/$app:$version
-if [ $? != 0 ]; then
-   echo "====push  image manifest failed!===="
-   exit 1
-fi
-sudo buildah rmi $prefix/$app:$version ||true
+IMAGE_NAME="$IMAGE_HUB_REGISTRY/$IMAGE_HUB_REPO/$IMAGE_KUBE:$IMAGE_TAG"
+
+sudo buildah rmi "$IMAGE_NAME" || true
+
+sudo buildah manifest create "$IMAGE_NAME"
+sudo buildah manifest add "$IMAGE_NAME" docker://"$IMAGE_NAME-amd64"
+sudo buildah manifest add "$IMAGE_NAME" docker://"$IMAGE_NAME-arm64"
+
+sudo buildah login --username "$IMAGE_HUB_USERNAME" --password "$IMAGE_HUB_PASSWORD" "$IMAGE_HUB_REGISTRY"
+sudo buildah manifest push --all "$IMAGE_NAME" docker://"$IMAGE_NAME"
+sudo sealos login "$IMAGE_HUB_REGISTRY" -u "$IMAGE_HUB_USERNAME" -p "$IMAGE_HUB_PASSWORD"
+
+sudo buildah rmi "$IMAGE_NAME" || true
