26a653646fdb3b88f09daee4f8a1f0da966abeda
muicoder <muicoder@gmail.com>
2022-08-30 10:53:07 +0800

    Name the image tag with the major version (#110)

--- .github/scripts/manifest.sh
+++ .github/scripts/manifest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -eu
 
 readonly CRI_TYPE=${criType}
 
@@ -27,9 +27,24 @@ sudo buildah rmi "$IMAGE_NAME" || true
 sudo buildah manifest create "$IMAGE_NAME"
 sudo buildah manifest add "$IMAGE_NAME" docker://"$IMAGE_NAME-amd64"
 sudo buildah manifest add "$IMAGE_NAME" docker://"$IMAGE_NAME-arm64"
+sudo buildah login -u "$IMAGE_HUB_USERNAME" -p "$IMAGE_HUB_PASSWORD" "$IMAGE_HUB_REGISTRY" &&
+  sudo buildah manifest push --all "$IMAGE_NAME" docker://"$IMAGE_NAME"
 
-sudo buildah login --username "$IMAGE_HUB_USERNAME" --password "$IMAGE_HUB_PASSWORD" "$IMAGE_HUB_REGISTRY"
-sudo buildah manifest push --all "$IMAGE_NAME" docker://"$IMAGE_NAME"
-sudo sealos login "$IMAGE_HUB_REGISTRY" -u "$IMAGE_HUB_USERNAME" -p "$IMAGE_HUB_PASSWORD"
+{
+  KUBE="$(echo "${IMAGE_TAG%%-*}" | cut -dv -f 2)"
+  if [[ $(wget -qO- "https://github.com/kubernetes/kubernetes/raw/master/CHANGELOG/CHANGELOG-${KUBE%.*}.md" |
+    grep -E '^- \[v[0-9\.]+\]' | awk '{print $2}' | awk -F\[ '{print $2}' | awk -F\] '{print $1}' |
+    cut -dv -f 2 | head -n 1) == "$KUBE" ]]; then
+    for IMAGE_NAME in \
+      "$IMAGE_HUB_REGISTRY/$IMAGE_HUB_REPO/$IMAGE_KUBE-dev:v${KUBE%.*}" \
+      "$IMAGE_HUB_REGISTRY/$IMAGE_HUB_REPO/$IMAGE_KUBE:v${KUBE%.*}"; do
+      sudo buildah manifest create "$IMAGE_NAME"
+      sudo buildah manifest add "$IMAGE_NAME" docker://"$IMAGE_NAME-amd64"
+      sudo buildah manifest add "$IMAGE_NAME" docker://"$IMAGE_NAME-arm64"
+      sudo buildah login -u "$IMAGE_HUB_USERNAME" -p "$IMAGE_HUB_PASSWORD" "$IMAGE_HUB_REGISTRY" &&
+        sudo buildah manifest push --all "$IMAGE_NAME" docker://"$IMAGE_NAME"
+    done
+  fi
+}
 
 sudo buildah rmi "$IMAGE_NAME" || true
