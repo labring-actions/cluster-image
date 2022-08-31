129067c42d5772ccd67b4c1d87faee8f52cc8ae8
muicoder <muicoder@gmail.com>
2022-08-31 14:58:06 +0800

    optimization manifest-other.sh scripts (#113)

--- .github/scripts/manifest-other.sh
+++ .github/scripts/manifest-other.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-readonly TYPE=${type}
+
+set -eu
+
+readonly ARCH=${arch}
 readonly IMAGE_NAME=${app?}
 readonly IMAGE_TAG=${version?}
 readonly IMAGE_HUB_REGISTRY=${registry?}
@@ -10,23 +13,22 @@ readonly IMAGE_NAME_FULL=${IMAGE_HUB_REGISTRY}/${IMAGE_HUB_REPO}/${IMAGE_NAME}:$
 
 # for host action ,Don't delete this code
 sudo buildah rmi "$IMAGE_NAME_FULL" || true
-sudo buildah manifest create "$IMAGE_NAME_FULL"
 
-case $TYPE in
-amd64)
-  sudo buildah manifest add "$IMAGE_NAME_FULL" docker://"$IMAGE_NAME_FULL-amd64"
-  ;;
-arm64)
-  sudo buildah manifest add "$IMAGE_NAME_FULL" docker://"$IMAGE_NAME_FULL-arm64"
+sudo buildah manifest create "$IMAGE_NAME_FULL"
+case $ARCH in
+amd64 | arm64)
+  sudo buildah manifest add "$IMAGE_NAME_FULL" docker://"$IMAGE_NAME_FULL-$ARCH"
   ;;
 *)
   sudo buildah manifest add "$IMAGE_NAME_FULL" docker://"$IMAGE_NAME_FULL-amd64"
   sudo buildah manifest add "$IMAGE_NAME_FULL" docker://"$IMAGE_NAME_FULL-arm64"
   ;;
 esac
+sudo buildah images
 
 sudo buildah login -u "$IMAGE_HUB_USERNAME" -p "$IMAGE_HUB_PASSWORD" "$IMAGE_HUB_REGISTRY" &&
-    sudo buildah manifest push --all "$IMAGE_NAME_FULL" docker://"$IMAGE_NAME_FULL" && echo "$IMAGE_NAME_FULL push success"
+  sudo buildah manifest push --all "$IMAGE_NAME_FULL" docker://"$IMAGE_NAME_FULL" && echo "$IMAGE_NAME_FULL push success"
 sudo buildah manifest inspect $IMAGE_NAME_FULL
-sudo buildah images
+
+# for host action ,Don't delete this code
 sudo buildah rmi "$IMAGE_NAME_FULL" || true
