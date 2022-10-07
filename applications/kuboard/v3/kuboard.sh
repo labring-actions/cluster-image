315b723395ce91b7175e5515416510503e36e12e
cuisongliu <cuisongliu@qq.com>
2022-10-08 00:57:52 +0800

    feature(main): add kuboard.sh
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- applications/kuboard/v3/kuboard.sh
+++ applications/kuboard/v3/kuboard.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+kubectl apply -f manifests/kuboard-v3.yaml
 PORT=$(kubectl -n kuboard get svc kuboard-v3 -ojsonpath='{.spec.ports[0].nodePort}')
 ADDRESS=$(kubectl get nodes -ojsonpath='{.items[0].status.addresses[0].address}')
 echo "visit http://$ADDRESS:$PORT, user: admin password: Kuboard123"
