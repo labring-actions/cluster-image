c275302d68f5827a6263f592e19755324d625633
cuisongliu <cuisongliu@qq.com>
2022-10-09 00:03:30 +0800

    feature(main): fix kubegems.sh
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- applications/kubegems/v1.21.4/kubegems.sh
+++ applications/kubegems/v1.21.4/kubegems.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 helm upgrade --install kubegems-installer --namespace kubegems-installer --create-namespace --set global.storageClass=local-hostpath --set installer.image.tag=v1.21.4 kubegems/kubegems-installer
-helm upgrade --install kubegems --namespace kubegems --create-namespace --set global.storageClass=local-hostpath --set global.imageRegistry=sealos.hub:5000 --set global.kubegemsVersion=v1.21.4 --set ingress.enable=false --set dashboard.service.type=NodePort  kubegems/kubegems
+helm upgrade --install kubegems --namespace kubegems --create-namespace --set global.storageClass=local-hostpath --set global.kubegemsVersion=v1.21.4 --set ingress.enable=false --set dashboard.service.type=NodePort  kubegems/kubegems
 
 PORT=$(kubectl -n kubegems get svc kubegems-dashboard -ojsonpath='{.spec.ports[0].nodePort}')
 ADDRESS=$(kubectl get nodes -ojsonpath='{.items[0].status.addresses[0].address}')
