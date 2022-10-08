47cd5be2cb3f2f2c3897bfc09d16404d6cfa7a72
cuisongliu <cuisongliu@qq.com>
2022-10-09 00:42:53 +0800

    feature(main): add  kuboard  v2
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- applications/kuboard/v3/kuboard.sh
+++ applications/kuboard/v3/kuboard.sh
@@ -4,4 +4,4 @@ kubectl apply -f manifests/kuboard-v3.yaml
 kubectl apply -f manifests/kuboard-shell.yaml
 PORT=$(kubectl -n kuboard get svc kuboard-v3 -ojsonpath='{.spec.ports[0].nodePort}')
 ADDRESS=$(kubectl get nodes -ojsonpath='{.items[0].status.addresses[0].address}')
-echo "visit http://$ADDRESS:$PORT, user: admin password: Kuboard123"
+echo "Visit Kuboard v3 : http://$ADDRESS:$PORT, user: admin password: Kuboard123"
