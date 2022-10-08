667f89bab6ec4dc22985fe2f60987c846662026a
cuisongliu <cuisongliu@qq.com>
2022-10-09 00:39:57 +0800

    feature(main): add dockerimage kuboard
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- dockerimages/kuboard/v2/init.sh
+++ dockerimages/kuboard/v2/init.sh
@@ -5,13 +5,13 @@ set -e
 readonly ARCH=${1:-amd64}
 
 if [ "$ARCH" = "amd64" ]; then
-cat <<EOF > Dockerfile
+cat <<EOF > Kubefile
 FROM eipwork/kuboard:latest
 EOF
 fi
 
 if [ "$ARCH" = "arm64" ]; then
-cat <<EOF > Dockerfile
+cat <<EOF > Kubefile
 FROM eipwork/kuboard:arm
 EOF
 fi
