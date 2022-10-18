bf58b5e7732b0d295b223e265bd5adc78f47075c
cuisongliu <cuisongliu@qq.com>
2022-10-18 09:18:19 +0800

    change Copyright sealos

--- docker/scripts/kubelet-post-stop.sh
+++ docker/scripts/kubelet-post-stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright © 2022 sealyun.
+# Copyright © 2022 sealos.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
