23e077205a0cfe8e76026de7f22cf898f55df5fe
willzhang <willzhmic@outlook.com>
2022-10-30 00:09:47 +0800

    update deepflow&apisix to latest
    Signed-off-by: willzhang <willzhmic@outlook.com>

--- applications/deepflow/latest/init.sh
+++ applications/deepflow/latest/init.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
-helm repo add deepflow https://deepflowys.github.io/deepflow-charts
-helm pull deepflow/deepflow --untar -d charts/
-arch=${1:-amd64}
-mkdir -p opt
-wget https://deepflow-ce.oss-accelerate.aliyuncs.com/bin/ctl/latest/linux/$arch/deepflow-ctl -O opt/deepflow-ctl
+
+set -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
+
+export readonly ARCH=${1:-amd64}
+export readonly NAME=${2:-$(basename "${PWD%/*}")}
+export readonly VERSION=${3:-$(basename "$PWD")}
+
+rm -rf opt/ charts/
+mkdir -p opt/ charts/
+wget https://deepflow-ce.oss-accelerate.aliyuncs.com/bin/ctl/latest/linux/${ARCH}/deepflow-ctl -O opt/deepflow-ctl
 chmod a+x opt/deepflow-ctl
-echo "download deepflow-ctl success"
+
+helm repo add deepflow --force-update https://deepflowys.github.io/deepflow-charts
+chart_version=`helm search repo --versions --regexp '\vdeepflow/deepflow\v' |grep ${VERSION} | awk '{print $2}' | sort -rn | head -n1`
+app_versions=`helm search repo --versions --regexp '\vdeepflow/deepflow\v' | awk '{print $3}' | grep -v VERSION`
+echo "support app_versions: $app_versions"
+
+if [ $VERSION = "latest" ]
+then helm pull deepflow/deepflow --untar -d charts/
+else helm pull deepflow/deepflow --version=${chart_version} --untar -d charts/
+fi
+
+cat <<EOF >>"Kubefile"
+FROM scratch
+COPY . .
+CMD ["mv opt/deepflow-ctl /usr/bin/","helm upgrade -i deepflow  charts/deepflow -n deepflow --create-namespace"]
+EOF
