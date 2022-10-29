cb3d34505b269b2d5301c0f114860b7ac0eb1942
will <willzhmic@outlook.com>
2022-10-30 00:20:59 +0800

    update deepflow&apisix to latest (#217)
    Signed-off-by: willzhang <willzhmic@outlook.com>
    
    Signed-off-by: willzhang <willzhmic@outlook.com>

---
+++ applications/apisix/latest/init.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
+
+export readonly ARCH=${1:-amd64}
+export readonly NAME=${2:-$(basename "${PWD%/*}")}
+export readonly VERSION=${3:-$(basename "$PWD")}
+
+helm repo add apisix --force-update https://charts.apiseven.com
+chart_version=`helm search repo --versions --regexp '\vapisix/apisix\v' |grep ${VERSION} | awk '{print $2}' | sort -rn | head -n1`
+app_versions=`helm search repo --versions --regexp '\vapisix/apisix\v' | awk '{print $3}' | grep -v VERSION`
+echo "support app_versions: $app_versions"
+
+rm -rf charts/ manifests/
+mkdir -p charts/ manifests/
+helm pull apisix/apisix --version=${charts_version} -d charts/ --untar
+
+helm template apisix apisix/apisix -n ingress-apisix --create-namespace \
+  --set gateway.type=NodePort,ingress-controller.enabled=true,dashboard.enabled=true,ingress-controller.config.apisix.serviceNamespace=ingress-apisix \
+  > manifests/apisix-template.yaml
+
+cat <<EOF >"Kubefile"
+FROM scratch
+COPY charts charts
+COPY registry registry
+CMD ["helm upgrade -i apisix charts/apisix -n ingress-apisix --create-namespace --set ingress-controller.enabled=true,dashboard.enabled=true,ingress-controller.config.apisix.serviceNamespace=ingress-apisix,gateway.type=NodePort,dashboard.service.type=NodePort"]
+EOF
