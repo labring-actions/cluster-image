804edaf18220cb5786491319707d1fbb8bb01507
will <willzhmic@outlook.com>
2022-11-06 14:08:32 +0800

    add harbor app v2.6.1 (#235)
    * add harbor app v2.6.1
    
    Signed-off-by: willzhang <willzhmic@outlook.com>
    
    * fix readme
    
    Signed-off-by: willzhang <willzhmic@outlook.com>
    
    Signed-off-by: willzhang <willzhmic@outlook.com>

---
+++ applications/harbor/latest/init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
+
+export readonly ARCH=${1:-amd64}
+export readonly NAME=${2:-$(basename "${PWD%/*}")}
+export readonly VERSION=${3:-$(basename "$PWD")}
+
+rm -rf charts/
+mkdir -p charts/
+
+# Remove `v` from image tag `vx.x.x`
+VERSION=`echo ${VERSION} | sed 's/.//'`
+chart_version=`helm search repo --versions --regexp '\vharbor/harbor\v' |grep ${VERSION} | awk '{print $2}' | sort -rn | head -n1`
+app_versions=`helm search repo --versions --regexp '\vharbor/harbor\v' | awk '{print $3}' | grep -v VERSION`
+echo "support app_versions: $app_versions"
+
+helm repo add harbor https://helm.goharbor.io
+helm pull harbor/harbor --version=${chart_version} -d charts/ --untar
+
+cat <<'EOF' >"Kubefile"
+FROM scratch
+ENV NODE_IP "core.harbor.domain"
+ENV NODE_PORT 30003
+COPY charts charts
+COPY registry registry
+CMD ["helm upgrade -i harbor charts/harbor -n harbor --create-namespace --set expose.type=nodePort,expose.tls.auto.commonName=$(NODE_IP),expose.nodePort.ports.https.nodePort=$(NODE_PORT),externalURL='https://$(NODE_IP):$(NODE_PORT)'"]
+EOF
