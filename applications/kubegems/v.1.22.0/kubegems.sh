71d00f54afcd61e984f4b08418c858a8ba778b58
LinkMaq <maqing1213@gmail.com>
2022-11-10 16:16:51 +0800

    kubegems v1.22

--- applications/kubegems/v.1.22.0/kubegems.sh
+++ applications/kubegems/v.1.22.0/kubegems.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-helm upgrade --install kubegems-installer --namespace kubegems-installer --create-namespace --set global.storageClass=local-hostpath --set installer.image.tag=v1.22.0-beta.2 kubegems/kubegems-installer
-helm upgrade --install kubegems --namespace kubegems --create-namespace --set global.storageClass=local-hostpath --set global.kubegemsVersion=v1.22.0-beta.2 --set ingress.enable=false --set dashboard.service.type=NodePort  kubegems/kubegems
+helm upgrade --install kubegems-installer --namespace kubegems-installer --create-namespace --set global.storageClass=local-hostpath --set installer.image.tag=v1.22.0 kubegems/kubegems-installer
+helm upgrade --install kubegems --namespace kubegems --create-namespace --set global.storageClass=local-hostpath --set global.kubegemsVersion=v1.22.0 --set ingress.enable=false --set dashboard.service.type=NodePort  kubegems/kubegems
 
 PORT=$(kubectl -n kubegems get svc kubegems-dashboard -ojsonpath='{.spec.ports[0].nodePort}')
 ADDRESS=$(kubectl get nodes -ojsonpath='{.items[0].status.addresses[0].address}')
