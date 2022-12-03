5a7009db503d52a424ec52775ea43d9957e6fc9a
willzhang <willzhmic@outlook.com>
2022-12-03 17:11:03 +0800

    update-app
    Signed-off-by: willzhang <willzhmic@outlook.com>

--- applications/fluxcd/latest/init.sh
+++ applications/fluxcd/latest/init.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-
-#set -e
+set -e
 
 cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
-
 export readonly ARCH=${1:-amd64}
 export readonly NAME=${2:-$(basename "${PWD%/*}")}
 export readonly VERSION=${3:-$(basename "$PWD")}
 
+app_version=$(echo ${VERSION} | sed 's/.//')
 rm -rf opt/ images/shim/
 rm -rf flux_*
 mkdir -p opt/ images/shim/
-wget -q https://github.com/fluxcd/flux2/releases/download/v${VERSION}/flux_${VERSION}_linux_amd64.tar.gz
-tar -zxvf flux_${VERSION}_linux_amd64.tar.gz -C opt/
-./opt/flux install --version v${VERSION} --export --components-extra="image-reflector-controller,image-automation-controller" |grep ghcr.io | awk '{print $2}' > images/shim/images-list.txt
+wget -q https://github.com/fluxcd/flux2/releases/download/v${app_version}/flux_${app_version}_linux_amd64.tar.gz
+tar -zxvf flux_${app_version}_linux_amd64.tar.gz -C opt/
+./opt/flux install --version v${app_version} --export --components-extra="image-reflector-controller,image-automation-controller" |grep ghcr.io | awk '{print $2}' > images/shim/images-list.txt
 
 cat <<EOF >"Kubefile"
 FROM scratch
 COPY opt opt
 COPY registry registry
-CMD ["cp opt/flux /usr/bin/","flux install --version v${VERSION} --components-extra='image-reflector-controller,image-automation-controller'"]
+CMD ["cp opt/flux /usr/bin/","flux install --version v${app_version} --components-extra='image-reflector-controller,image-automation-controller'"]
 EOF
