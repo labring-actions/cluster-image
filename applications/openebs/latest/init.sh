1cb68384bf0adf4bd4577c0be65c07ffee18d5ef
willzhang <willzhmic@outlook.com>
2022-12-10 18:04:05 +0800

    update init.sh
    Signed-off-by: willzhang <willzhmic@outlook.com>

--- applications/openebs/latest/init.sh
+++ applications/openebs/latest/init.sh
@@ -7,12 +7,11 @@ export readonly NAME=${2:-$(basename "${PWD%/*}")}
 export readonly VERSION=${3:-$(basename "$PWD")}
 
 helm repo add openebs https://openebs.github.io/charts
-app_version=`echo ${VERSION} | sed 's/.//'`
-chart_version=`helm search repo --versions --regexp '\vopenebs/openebs\v' |grep ${app_version} | awk '{print $2}' | sort -rn | head -n1`
-rm -rf charts/ 
+chart_version=`helm search repo --versions --regexp '\vopenebs/openebs\v' |grep ${VERSION#v} | awk '{print $2}' | sort -rn | head -n1`
+rm -rf charts/
 helm pull openebs/openebs --version=${chart_version} -d charts/ --untar
 utils_tag=$(helm show values charts/openebs --jsonpath {.helper.imageTag})
-mkdir -p images/shim 
+mkdir -p images/shim
 echo "openebs/linux-utils:$utils_tag" >images/shim/openebsImages
 
 cat <<'EOF' >"Kubefile"
