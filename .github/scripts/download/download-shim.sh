60189a6a9c6e6af26733d44af1e82c324ebacb5d
cuisongliu <cuisongliu@qq.com>
2022-12-13 00:04:46 +0800

    feature(main): add tmp for image cri
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- .github/scripts/download/download-shim.sh
+++ .github/scripts/download/download-shim.sh
@@ -10,8 +10,8 @@ mkdir -p "$ROOT"
 
 cd "$ROOT" && {
   sudo buildah from --name "sealos-v$SEALOS-$ARCH" "ghcr.io/labring-actions/cache:sealos-v$SEALOS-$ARCH"
-  sudo cp -a "$(sudo buildah mount "sealos-v$SEALOS-$ARCH")"/image-cri-shim .
-  sudo cp -a "$(sudo buildah mount "sealos-v$SEALOS-$ARCH")"/sealctl .
+  sudo cp -a "$(sudo buildah mount "sealos-v$SEALOS-$ARCH")"/v$SEALOS/image-cri-shim .
+  sudo cp -a "$(sudo buildah mount "sealos-v$SEALOS-$ARCH")"/v$SEALOS/sealctl .
   sudo buildah umount "sealos-v$SEALOS-$ARCH"
 }
 
