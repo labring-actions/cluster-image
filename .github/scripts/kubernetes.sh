0522b8eb46e1be3412418b6c0e3c756db3cb2d93
muicoder <muicoder@gmail.com>
2022-12-13 16:03:46 +0800

    cache registry for k8s

--- .github/scripts/kubernetes.sh
+++ .github/scripts/kubernetes.sh
@@ -21,6 +21,7 @@ readonly binDIR="/tmp/$(whoami)/bin"
 {
   FROM_KUBE=$(sudo buildah from "ghcr.io/labring-actions/cache:kubernetes-v$KUBE-amd64")
   sudo cp -a "$(sudo buildah mount "$FROM_KUBE")"/kube/kubeadm "$binDIR/kubeadm"
+  sudo cp -a "$(sudo buildah mount "$FROM_KUBE")"/kube/registry "$ROOT"
   sudo buildah umount "$FROM_KUBE"
   sudo chown "$(whoami)" "$binDIR"/*
   chmod a+x "$binDIR"/*
