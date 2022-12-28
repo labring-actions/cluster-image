28542611cebbace0a9c62ad4c0d62cc0e9cafaf5
LinkMaq <maqing1213@gmail.com>
2022-12-28 17:15:14 +0800

    install kubegems by installer not helm

--- applications/kubegems/v1.22.0/init.sh
+++ applications/kubegems/v1.22.0/init.sh
@@ -14,6 +14,7 @@ helm pull kubegems/kubegems --version=${VERSION#v} --untar -d charts/
 cat <<EOF >"Kubefile"
 FROM scratch
 COPY registry ./registry
+COPY manifest ./manifest
 COPY charts ./charts
-CMD ["helm upgrade -i kubegems-installer charts/kubegems-installer -n kubegems-installer --create-namespace --set installer.image.tag=${VERSION}","helm upgrade -i kubegems charts/kubegems -n kubegems --create-namespace --set global.kubegemsVersion=${VERSION},ingress.enable=false,dashboard.service.type=NodePort"]
+CMD ["helm upgrade -i kubegems-installer charts/kubegems-installer -n kubegems-installer --create-namespace --set installer.image.tag=${VERSION}","kubectl apply -f manifest/kubegems.yaml"]
 EOF
