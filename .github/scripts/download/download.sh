675e1a9c1ed254cec19b6b40f0d752aba45cebf5
muicoder <muicoder@gmail.com>
2022-12-28 10:43:53 +0800

    Update download.sh

--- .github/scripts/download/download.sh
+++ .github/scripts/download/download.sh
@@ -4,7 +4,7 @@ set -e
 
 readonly ARCH=amd64
 readonly SEALOS=${sealoslatest:-$(
-  until curl -sL "https://api.github.com/repos/labring/sealos/releases/latest"; do sleep 3; done | grep tarball_url | awk -F\" '{print $(NF-1)}' | awk -F/ '{print $NF}' | cut -dv -f2
+  until curl -sL "https://api.github.com/repos/labring/sealos/releases/latest" | grep tarball_url; do sleep 3; done | awk -F\" '{print $(NF-1)}' | awk -F/ '{print $NF}' | cut -dv -f2
 )}
 readonly IMAGE_CACHE_NAME="ghcr.io/labring-actions/cache"
 readonly ROOT="/tmp/$(whoami)/bin"
