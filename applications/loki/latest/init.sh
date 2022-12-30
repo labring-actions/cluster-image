35823e93b032d0784f99827d8032db135b6d541b
will <willzhmic@outlook.com>
2022-12-30 18:34:39 +0800

    update loki and calico (#285)

--- applications/loki/latest/init.sh
+++ applications/loki/latest/init.sh
@@ -6,7 +6,7 @@ export readonly ARCH=${1:-amd64}
 export readonly NAME=${2:-$(basename "${PWD%/*}")}
 export readonly VERSION=${3:-$(basename "$PWD")}
 
-helm repo add loki https://grafana.github.io/loki/charts
+helm repo add grafana https://grafana.github.io/loki/charts
 chart_version=`helm search repo --versions --regexp '\vgrafana/loki-stack\v' |grep ${VERSION} | awk '{print $2}' | sort -rn | head -n1`
 rm -rf charts/
 helm pull grafana/loki-stack --version=${chart_version} -d charts/ --untar
