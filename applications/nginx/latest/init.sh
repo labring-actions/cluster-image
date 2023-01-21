5d6da4a060ad7e5f7239516b6309a17f1c5275d1
willzhang <willzhmic@outlook.com>
2023-01-21 15:17:54 +0800

    update nginx
    Signed-off-by: willzhang <willzhmic@outlook.com>

--- applications/nginx/latest/init.sh
+++ applications/nginx/latest/init.sh
@@ -1,29 +1,16 @@
-#!/bin/bash
-
-#set -e
+#!/usr/bin/env bash
+set -e
 
 cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
-
 export readonly ARCH=${1:-amd64}
 export readonly NAME=${2:-$(basename "${PWD%/*}")}
 export readonly VERSION=${3:-$(basename "$PWD")}
 
-helm repo add bitnami https://charts.bitnami.com/bitnami
-chart_version=`helm search repo --versions --regexp '\vbitnami/nginx\v' |grep ${VERSION} | awk '{print $2}' | sort -rn | head -n1`
-app_versions=`helm search repo --versions --regexp '\vbitnami/nginx\v' | awk '{print $3}' | grep -v VERSION`
+repo_url="https://charts.bitnami.com/bitnami"
+repo_name="bitnami/nginx"
+chart_name="nginx"
 
-echo $app_versions |grep $VERSION
-if [[ $? -ne 0 ]]; then
-  echo "The version number does not exist!,support version number are: $app_versions"
-  exit 1
-fi
-
-rm -rf charts
-mkdir -p charts
-helm pull bitnami/nginx --version=${chart_version} --untar -d charts
-
-cat <<EOF >>"Kubefile"
-FROM scratch
-COPY . .
-CMD ["helm upgrade -i nginx charts/nginx -n nginx --create-namespace --set service.type=NodePort"]
-EOF
+helm repo add ${chart_name} ${repo_url}
+chart_version=$(helm search repo --versions --regexp "\v"${repo_name}"\v" |grep ${VERSION#v} | awk '{print $2}' | sort -rn | head -n1)
+helm pull ${repo_name} --version=${chart_version} -d charts --untar
+yq e -i '.service.type="NodePort"' charts/nginx/values.yaml
