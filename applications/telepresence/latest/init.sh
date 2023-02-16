a18ff9630b687efc4c0747e695805a59df9a60a4
cuisongliu <cuisongliu@qq.com>
2023-02-16 11:37:49 +0800

    add telepresence

---
+++ applications/telepresence/latest/init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
+
+export readonly ARCH=${1:-amd64}
+export readonly NAME=${2:-$(basename "${PWD%/*}")}
+export readonly VERSION=${3:-$(basename "$PWD")}
+
+rm -rf charts/ images/shim/
+mkdir -p charts/ images/shim/
+
+# Remove `v` from image tag `vx.x.x`
+chart_version=`echo ${VERSION} | sed 's/.//'`
+
+helm repo add datawire  https://app.getambassador.io
+helm pull datawire/telepresence --version=${chart_version} -d charts/ --untar
+
+cat <<EOF >"Kubefile"
+FROM scratch
+COPY charts charts
+COPY registry registry
+CMD ["helm upgrade -i traffic-manager charts/telepresence -n ambassador --create-namespace"]
+EOF
