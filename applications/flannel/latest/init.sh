bb26a5473297fcb50ccf33acdba32efdde1dcc30
willzhang <willzhmic@outlook.com>
2023-03-26 00:46:48 +0800

    fix cni_plugin_image
    Signed-off-by: willzhang <willzhmic@outlook.com>

--- applications/flannel/latest/init.sh
+++ applications/flannel/latest/init.sh
@@ -19,8 +19,7 @@ yq e -i '.podCidr="100.64.0.0/10"' charts/flannel/values.yaml
 readonly cni_latest_version=v${cnilatest:-$(
   until curl -sL "https://api.github.com/repos/containernetworking/plugins/releases/latest"; do sleep 3; done | grep tarball_url | awk -F\" '{print $(NF-1)}' | awk -F/ '{print $NF}' | cut -dv -f2
 )}
-cni_plugin_image="registry.cn-shenzhen.aliyuncs.com/cnmirror/docker-cni-plugins:${cni_latest_version}"
-#cni_plugin_image="docker.io/labring/docker-cni-plugins:${cni_latest_version}"
+cni_plugin_image="docker.io/labring/docker-cni-plugins:${cni_latest_version}"
 cat << EOF | sed -i '/^\s*initContainers/ r /dev/stdin' charts/flannel/templates/daemonset.yaml
       - name: install-cni-plugin-sealos
         image: "${cni_plugin_image}"
