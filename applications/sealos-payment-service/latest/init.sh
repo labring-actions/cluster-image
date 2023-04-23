b3bbc8d8f28d2d5430416cd6a8cf97426b6179ad
cuisongliu <cuisongliu@qq.com>
2023-04-23 15:50:27 +0800

    feature(main): delete sealos images
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- applications/sealos-payment-service/latest/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git clone https://github.com/labring/sealos.git
-cp -rf sealos/service/payment/deploy/* .
-rm -rf sealos
-tree -L 3
-if  [ -f Dockerfile ]; then
-  mv Dockerfile Kubefile
-fi
