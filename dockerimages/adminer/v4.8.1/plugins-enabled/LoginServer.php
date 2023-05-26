bc12b4e330f789acc0495e75ad7d10fd67a31166
zzjin <zzjin@users.noreply.github.com>
2023-05-26 21:32:16 +0800

    Adminer docker image update. (#348)

--- dockerimages/adminer/v4.8.1/plugins-enabled/LoginServer.php
+++ dockerimages/adminer/v4.8.1/plugins-enabled/LoginServer.php
@@ -2,8 +2,8 @@
 require_once( 'plugins/ClickLogin.php' );
 
 $initServers = [];
-if ( file_exists( 'servers.php' ) ) {
-	$initServers = include( 'servers.php' );
+if ( file_exists( 'servers/servers.php' ) ) {
+	$initServers = include( 'servers/servers.php' );
 }
 
 return new ClickLogin( $initServers );
