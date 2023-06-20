1aeaa88d0a3887ab5b5875653e1884a791550889
cuisongliu <cuisongliu@qq.com>
2023-06-20 23:43:06 +0800

    feature(main): add openIM
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- applications/openIM/errorcode/init.sh
+++ applications/openIM/errorcode/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p manifests
-helm template openim openIM  --values openIM/values.yaml --values openIM/configmap-config.values.yaml --values openIM/configmap-notification.values.yaml --values openIM/configmap-password.values.yaml  --debug > manifests/openIM.yaml
+helm template openim openIM  --values openIM/values.yaml  --values openIM/configmap-notification.values.yaml --values openIM/configmap-password.values.yaml  --debug > manifests/openIM.yaml
