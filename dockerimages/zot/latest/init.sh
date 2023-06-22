37a0faee941131476413a85968fe38eecf618628
cuisongliu <cuisongliu@qq.com>
2023-06-22 15:33:46 +0800

    feature(main): add ZOT
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- dockerimages/zot/latest/init.sh
+++ dockerimages/zot/latest/init.sh
@@ -5,6 +5,6 @@ set -e
 readonly ARCH=${1:-amd64}
 readonly NAME=${2:-zot}
 readonly VERSION=${3:-v2.0.0-rc4}
-cat <<EOF > Kubefile
+cat <<EOF > Dockerfile
 FROM ghcr.io/project-zot/zot-linux-${ARCH}:${VERSION}
 EOF
