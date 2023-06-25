4de1083cdf1202fe3c911ece708cbb444b5f55a3
cuisongliu <cuisongliu@qq.com>
2023-06-26 00:05:36 +0800

    feature(main): add kb
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- .github/scripts/download.sh
+++ .github/scripts/download.sh
@@ -10,16 +10,16 @@ readonly IMAGE_CACHE_NAME="ghcr.io/labring-actions/cache"
 readonly ROOT="/tmp/$(whoami)/bin"
 mkdir -p "$ROOT"
 
-until sudo docker run --rm -v "/usr/bin:/pwd" -w /tools --entrypoint /bin/sh "$IMAGE_CACHE_NAME:tools-amd64" -c "cp -a buildah /pwd"; do
+until sudo podman run --rm -v "/usr/bin:/pwd" -w /tools --entrypoint /bin/sh "$IMAGE_CACHE_NAME:tools-amd64" -c "cp -a buildah /pwd"; do
   sleep 1
 done
 
 cd "$ROOT" && {
-  docker run --rm -v "$PWD:/pwd" -w /tools --entrypoint /bin/sh "$IMAGE_CACHE_NAME:tools-$ARCH" -c "cp -a . /pwd"
+  podman run --rm -v "$PWD:/pwd" -w /tools --entrypoint /bin/sh "$IMAGE_CACHE_NAME:tools-$ARCH" -c "cp -a . /pwd"
   if [[ -n "$sealosPatch" ]]; then
-    docker run --rm -v "$PWD:/pwd" -w /usr/bin --entrypoint /bin/sh ghcr.io/labring/sealos:dev -c "cp -a sealos /pwd"
+    podman run --rm -v "$PWD:/pwd" -w /usr/bin --entrypoint /bin/sh ghcr.io/labring/sealos:dev -c "cp -a sealos /pwd"
   else
-    docker run --rm -v "$PWD:/pwd" -w /sealos --entrypoint /bin/sh "$IMAGE_CACHE_NAME:sealos-v$SEALOS-$ARCH" -c "cp -a sealos /pwd"
+    podman run --rm -v "$PWD:/pwd" -w /sealos --entrypoint /bin/sh "$IMAGE_CACHE_NAME:sealos-v$SEALOS-$ARCH" -c "cp -a sealos /pwd"
   fi
   sudo chown "$(whoami)" ./*
 }
