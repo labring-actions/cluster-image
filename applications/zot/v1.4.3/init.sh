d38346dc8058331e30994e67d10ea8138863dbac
cuisongliu <cuisongliu@qq.com>
2023-06-25 12:26:02 +0800

    feature(main): add zot
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- applications/zot/v1.4.3/init.sh
+++ applications/zot/v1.4.3/init.sh
@@ -6,19 +6,14 @@ export readonly ARCH=${1:-amd64}
 export readonly NAME=${2:-$(basename "${PWD%/*}")}
 export readonly VERSION=${3:-$(basename "$PWD")}
 
-cat <<EOF > zot/"values-version.yaml"
-image:
-  tag: "${VERSION}"
-EOF
-
 mkdir -p "charts"
 mkdir -p "manifests"
-helm template zot zot  --values zot/values.yaml --values  zot/values-version.yaml  --debug > manifests/zot.yaml
+helm template zot zot  --values zot/values.yaml --values  zot/values-version.yaml --set image.tag=${VERSION}  --debug > manifests/zot.yaml
 
 cp -rf zot charts/
 cat <<EOF >"Kubefile"
 FROM scratch
 COPY charts charts
 COPY registry registry
-CMD ["helm upgrade --install zot charts/zot --namespace zot --create-namespace --values charts/zot/values.yaml --values charts/zot/values-version.yaml"]
+CMD ["helm upgrade --install zot charts/zot --namespace zot --create-namespace --values charts/zot/values.yaml --set image.tag=${VERSION}"]
 EOF
