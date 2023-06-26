ca818dc9bce3a3069a4889c8f0b99e9d9469e092
cuisongliu <cuisongliu@qq.com>
2023-06-26 19:20:45 +0800

    feature(main): add kb
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- .github/scripts/manifest-docker.sh
+++ .github/scripts/manifest-docker.sh
@@ -16,23 +16,23 @@ sudo docker rmi "$IMAGE_NAME_FULL" || true
 set -ex
 case $ARCH in
 amd64 | arm64)
-  sudo podman pull "$IMAGE_NAME_FULL-$ARCH"
-  sudo podman manifest create "$IMAGE_NAME_FULL" "$IMAGE_NAME_FULL-$ARCH"
-  sudo podman manifest annotate "$IMAGE_NAME_FULL" "$IMAGE_NAME_FULL-$ARCH" --arch $ARCH
+  sudo docker pull "$IMAGE_NAME_FULL-$ARCH"
+  sudo docker manifest create "$IMAGE_NAME_FULL" "$IMAGE_NAME_FULL-$ARCH"
+  sudo docker manifest annotate "$IMAGE_NAME_FULL" "$IMAGE_NAME_FULL-$ARCH" --arch $ARCH
   ;;
 *)
-  sudo podman pull "$IMAGE_NAME_FULL-amd64"
-  sudo podman pull "$IMAGE_NAME_FULL-arm64"
-  sudo podman manifest create "$IMAGE_NAME_FULL" "$IMAGE_NAME_FULL-amd64" "$IMAGE_NAME_FULL-arm64"
-  sudo podman manifest annotate "$IMAGE_NAME_FULL" "$IMAGE_NAME_FULL-amd64" --arch amd64
-  sudo podman manifest annotate "$IMAGE_NAME_FULL" "$IMAGE_NAME_FULL-arm64" --arch arm64
+  sudo docker pull "$IMAGE_NAME_FULL-amd64"
+  sudo docker pull "$IMAGE_NAME_FULL-arm64"
+  sudo docker manifest create "$IMAGE_NAME_FULL" "$IMAGE_NAME_FULL-amd64" "$IMAGE_NAME_FULL-arm64"
+  sudo docker manifest annotate "$IMAGE_NAME_FULL" "$IMAGE_NAME_FULL-amd64" --arch amd64
+  sudo docker manifest annotate "$IMAGE_NAME_FULL" "$IMAGE_NAME_FULL-arm64" --arch arm64
   ;;
 esac
-sudo podman images
+sudo docker images
 
-sudo podman login -u "$IMAGE_HUB_USERNAME" -p "$IMAGE_HUB_PASSWORD" "$IMAGE_HUB_REGISTRY" &&
-  sudo podman manifest push "$IMAGE_NAME_FULL"  && echo "$IMAGE_NAME_FULL push success"
-sudo podman manifest inspect $IMAGE_NAME_FULL
+sudo docker login -u "$IMAGE_HUB_USERNAME" -p "$IMAGE_HUB_PASSWORD" "$IMAGE_HUB_REGISTRY" &&
+  sudo docker manifest push "$IMAGE_NAME_FULL"  && echo "$IMAGE_NAME_FULL push success"
+sudo docker manifest inspect $IMAGE_NAME_FULL
 
 # for host action ,Don't delete this code
-sudo podman rmi "$IMAGE_NAME_FULL" || true
+sudo docker rmi "$IMAGE_NAME_FULL" || true
