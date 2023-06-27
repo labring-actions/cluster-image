070d1c48ae9ae625808c9d031c23338ddb3922b1
cuisongliu <cuisongliu@qq.com>
2023-06-27 21:51:19 +0800

    feature(main): add kb
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- applications/kubeblocks/v0.5.3/kb.sh
+++ applications/kubeblocks/v0.5.3/kb.sh
@@ -2,4 +2,4 @@
 bash for-range.sh
 export ZOT_PORT=$(kubectl get --namespace zot -o jsonpath="{.spec.ports[0].port}" services zot)
 export ZOT_IP=$(kubectl get --namespace zot -o jsonpath="{.spec.clusterIP}" services zot)
-helm upgrade -i kubeblocks oci://"$ZOT_IP":"$ZOT_PORT"/helm-charts/kubeblocks  --set snapshot-controller.enable=false, --insecure-skip-tls-verify  -n kb-system --create-namespace
+helm upgrade -i kubeblocks oci://"$ZOT_IP":"$ZOT_PORT"/helm-charts/kubeblocks  --set snapshot-controller.enable=false,addonChartLocationBase=oci://zot.zot.svc.cluster.local:8443/helm-charts --insecure-skip-tls-verify  -n kb-system --create-namespace
