20b6571ac01430a7fe65b0c34ad14210b875f71a
cuisongliu <cuisongliu@qq.com>
2023-07-16 15:08:51 +0800

    feature(main): add sealos latest
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- applications/sealos/latest/init.sh
+++ applications/sealos/latest/init.sh
@@ -6,7 +6,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
 
 export readonly ARCH=${1:-amd64}
 export readonly NAME=${2:-$(basename "${PWD%/*}")}
-export readonly VERSION=${3:-$(basename "$PWD")}
+export readonly VERSION=${3:-latest}
 
 mkdir -p images/shim
 echo "ghcr.io/labring/sealos:$VERSION" > images/shim/sealosImage
