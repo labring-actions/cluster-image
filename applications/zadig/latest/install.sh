434b18acc3631e7b660e4dccfe2eb550b2277181
cuisongliu <cuisongliu@qq.com>
2023-08-22 10:00:02 +0800

    feature(main): add zadig
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- applications/zadig/latest/install.sh
+++ applications/zadig/latest/install.sh
@@ -8,7 +8,7 @@ helm upgrade --install zadig charts/zadig --namespace zadig --create-namespace  
     --set endpoint.IP=${IP} \
     --set tags.minio=true \
     --set gloo.gatewayProxies.gatewayProxy.service.httpNodePort=${PORT} \
-    --set global.extensions.extAuth.extauthzServerRef.namespace=zadig-system \
+    --set global.extensions.extAuth.extauthzServerRef.namespace=zadig \
     --set gloo.gatewayProxies.gatewayProxy.service.type=NodePort \
     --set "dex.config.staticClients[0].redirectURIs[0]=http://${IP}:${PORT}/api/v1/callback,dex.config.staticClients[0].id=zadig,dex.config.staticClients[0].name=zadig,dex.config.staticClients[0].secret=ZXhhbXBsZS1hcHAtc2VjcmV0" \
     ${HELM_OPTS}
