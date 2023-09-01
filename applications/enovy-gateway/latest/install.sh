e9874e1da429bbb28afb7345bdc23759695b2a44
cuisongliu <cuisongliu@qq.com>
2023-09-01 13:23:09 +0800

    feature(main): add gateway enovy
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- applications/enovy-gateway/latest/install.sh
+++ applications/enovy-gateway/latest/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 HELM_OPTS="${HELM_OPTS:-}"
 
-helm upgrade --install eg charts/gateway-helm --namespace envoy-gateway-system --create-namespace   ${HELM_OPTS}
+helm upgrade --install eg charts/gateway-helm --namespace gateway-system --create-namespace   ${HELM_OPTS}
