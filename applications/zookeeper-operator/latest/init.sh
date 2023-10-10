c391cb45b774bce1913ece113ff7b4d566718589
cuisongliu <cuisongliu@qq.com>
2023-10-10 23:40:35 +0800

    feature(main): add  zk-operator
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

---
+++ applications/zookeeper-operator/latest/init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
+export readonly ARCH=${1:-amd64}
+export readonly NAME=${2:-$(basename "${PWD%/*}")}
+export readonly VERSION=${3:-$(basename "$PWD")}
+
+rm -rf charts && mkdir -p charts
+helm repo add pravega https://charts.pravega.io
+chart_version=`helm search repo --versions --regexp '\vpravega/zookeeper-operator\v' |grep ${VERSION#v} | awk '{print $2}' | sort -rn | head -n1`
+helm pull pravega/zookeeper-operator --version=${chart_version} -d charts/ --untar
+helm pull pravega/zookeeper --version=${chart_version} -d charts/ --untar
---
+++ applications/zookeeper-operator/latest/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+IP="${IP:-127.0.0.1}"
+PORT="${PORT:-30080}"
+OPERATOR_HELM_OPTS="${OPERATOR_HELM_OPTS:-}"
+HELM_OPTS="${HELM_OPTS:-}"
+
+helm upgrade --install zook-operator charts/zookeeper-operator --namespace zook-system --create-namespace  ${OPERATOR_HELM_OPTS}
+helm upgrade --install zook charts/zookeeper --namespace zook --create-namespace  ${HELM_OPTS}
