2dab04bcaa8e250ecbcb53f8fe8442af14860a8b
cuisongliu <cuisongliu@qq.com>
2023-10-11 00:08:55 +0800

    feature(main): add  zk-operator
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- applications/zookeeper-operator/latest/install.sh
+++ applications/zookeeper-operator/latest/install.sh
@@ -6,4 +6,4 @@ OPERATOR_HELM_OPTS="${OPERATOR_HELM_OPTS:-}"
 HELM_OPTS="${HELM_OPTS:-}"
 
 helm upgrade --install zook-operator charts/zookeeper-operator --namespace zook-system --create-namespace  ${OPERATOR_HELM_OPTS}
-helm upgrade --install zook charts/zookeeper --namespace zook --create-namespace  ${HELM_OPTS}
+helm upgrade --install zook charts/zookeeper --namespace zook-system --create-namespace  ${HELM_OPTS}
