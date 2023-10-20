5c687273ffc802d47d38fb458be4e7bec899cf53
willzhang <willzhmic@outlook.com>
2023-10-20 15:22:47 +0800

    fix flannel namespace name error
    Signed-off-by: willzhang <willzhmic@outlook.com>

--- applications/flannel/latest/entrypoint.sh
+++ applications/flannel/latest/entrypoint.sh
@@ -5,8 +5,9 @@ cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
 
 NAME=${NAME:-"flannel"}
 NAMESPACE=${NAMESPACE:-"kube-flannel"}
+CHARTS=${CHARTS:-"./charts/flannel"}
 HELM_OPTS=${HELM_OPTS:-""}
 
-kubectl create ns ${NAMESAPCE} > /dev/null 2>&1 || true
-kubectl label --overwrite ns ${NAMESAPCE} pod-security.kubernetes.io/enforce=privileged
-helm upgrade -i ${NAME} ./charts/flannel -n ${NAMESAPCE} --create-namespace ${HELM_OPTS}
+kubectl create ns ${NAMESPACE} > /dev/null 2>&1 || true
+kubectl label --overwrite ns ${NAMESPACE} pod-security.kubernetes.io/enforce=privileged
+helm upgrade -i ${NAME} ${CHARTS} -n ${NAMESPACE} --create-namespace ${HELM_OPTS}
