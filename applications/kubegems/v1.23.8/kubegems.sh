b3f18c64bde8cb55e10d613e9a224d0bc4fc1595
willzhang <willzhmic@outlook.com>
2023-10-27 17:17:18 +0800

    update kubegems HELM_OPTS
    Signed-off-by: willzhang <willzhmic@outlook.com>

--- applications/kubegems/v1.23.8/kubegems.sh
+++ applications/kubegems/v1.23.8/kubegems.sh
@@ -4,7 +4,7 @@ NAME=${NAME:-"kubegems"}
 NAMESPACE=${NAMESPACE:-"kubegems"}
 VERSION=${VERSION:-"1.23.8"}
 CHARTS=${CHARTS:-"./chart/kubegems"}
-HELM_OPTS=${HELM_OPTS:-"--set ingress.enable=false --set dashboard.service.type=NodePort"}
+HELM_OPTS=${HELM_OPTS:-"--set ingress.enabled=false --set dashboard.service.type=NodePort"}
 
 helm upgrade -i kubegems-installer ./chart/kubegems-installer --version=${VERSION} -n kubegems-installer --create-namespace --wait
 helm upgrade -i ${NAME} ${CHARTS} --version=${VERSION} -n ${NAMESPACE} --create-namespace ${HELM_OPTS}
