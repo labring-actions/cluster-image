ac65edfac45c67965199eb046c8d1ad6fdfab3a5
huangzhangshu <huangzhangshu@apecloud.com>
2023-11-08 11:09:38 +0800

    chore: change app.sh

--- .github/scripts/apps.sh
+++ .github/scripts/apps.sh
@@ -24,9 +24,28 @@ fi
 cp -rf "applications/$APP_NAME/$APP_VERSION"/* $buildDir
 
 cd $buildDir && {
+    [[ -s Dockerfile ]] && Kubefile="Dockerfile" || Kubefile="Kubefile"
+
+    if [[ -s "build_arch" ]]; then
+        FILE_CONTENT=$(cat "build_arch"| tr -d '[:space:]'| tr -d '\n'| tr -d '\t')
+        if [[ "$FILE_CONTENT" != "$APP_ARCH" ]]; then
+            echo "The content of build_arch does not match the ARCH variable. Exiting."
+            exit 0
+        fi
+    fi
     if [[ -s init.sh ]]; then
         bash init.sh "$APP_ARCH" "$APP_NAME" "$APP_VERSION"
     fi
+
     IMAGE_NAME="$IMAGE_HUB_REGISTRY/$IMAGE_HUB_REPO/$APP_NAME:$APP_VERSION-$APP_ARCH"
-    echo "init $IMAGE_NAME success"
+
+    IMAGE_BUILD="${IMAGE_NAME%%:*}:build-$(date +%s)"
+    sudo sealos build -t "$IMAGE_BUILD" --max-pull-procs=10 --isolation=chroot --platform "linux/$APP_ARCH" -f $Kubefile .
+
+    sudo sealos tag "$IMAGE_BUILD" "$IMAGE_NAME" && sudo sealos rmi -f "$IMAGE_BUILD"
+
+    sudo sealos login -u "$IMAGE_HUB_USERNAME" -p "$IMAGE_HUB_PASSWORD" "$IMAGE_HUB_REGISTRY" &&
+    sudo sealos push "$IMAGE_NAME" && echo "$IMAGE_NAME push success"
 }
+
+sudo buildah images
