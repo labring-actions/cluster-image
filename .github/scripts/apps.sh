a6322d54a842742234f373d2a1e253801d0549f3
huangzhangshu <huangzhangshu@apecloud.com>
2023-11-08 10:20:15 +0800

    chore: change docker buildx push

--- .github/scripts/apps.sh
+++ .github/scripts/apps.sh
@@ -6,10 +6,10 @@ readonly APP_NAME=${app?}
 readonly APP_VERSION=${version?}
 readonly APP_ARCH=${arch?}
 
-readonly IMAGE_HUB_REGISTRY=${registry?}
-readonly IMAGE_HUB_REPO=${repo?}
-readonly IMAGE_HUB_USERNAME=${username?}
-readonly IMAGE_HUB_PASSWORD=${password?}
+readonly IMAGE_HUB_REGISTRY=${DOCKER_REGISTRY_URL?}
+readonly IMAGE_HUB_REPO=${DOCKER_REPO?}
+readonly IMAGE_HUB_USERNAME=${DOCKER_USER?}
+readonly IMAGE_HUB_PASSWORD=${DOCKER_PASSWORD?}
 
 readonly buildDir=.build-image
 
@@ -24,28 +24,9 @@ fi
 cp -rf "applications/$APP_NAME/$APP_VERSION"/* $buildDir
 
 cd $buildDir && {
-    [[ -s Dockerfile ]] && Kubefile="Dockerfile" || Kubefile="Kubefile"
-
-    if [[ -s "build_arch" ]]; then
-        FILE_CONTENT=$(cat "build_arch"| tr -d '[:space:]'| tr -d '\n'| tr -d '\t')
-        if [[ "$FILE_CONTENT" != "$APP_ARCH" ]]; then
-            echo "The content of build_arch does not match the ARCH variable. Exiting."
-            exit 0
-        fi
-    fi
     if [[ -s init.sh ]]; then
         bash init.sh "$APP_ARCH" "$APP_NAME" "$APP_VERSION"
     fi
-
     IMAGE_NAME="$IMAGE_HUB_REGISTRY/$IMAGE_HUB_REPO/$APP_NAME:$APP_VERSION-$APP_ARCH"
-
-    IMAGE_BUILD="${IMAGE_NAME%%:*}:build-$(date +%s)"
-    sudo sealos build -t "$IMAGE_BUILD" --isolation=chroot --platform "linux/$APP_ARCH" -f $Kubefile .
-
-    sudo sealos tag "$IMAGE_BUILD" "$IMAGE_NAME" && sudo sealos rmi -f "$IMAGE_BUILD"
-
-    sudo sealos login -u "$IMAGE_HUB_USERNAME" -p "$IMAGE_HUB_PASSWORD" "$IMAGE_HUB_REGISTRY" &&
-    sudo sealos push "$IMAGE_NAME" && echo "$IMAGE_NAME push success"
+    echo "init $IMAGE_NAME success"
 }
-
-sudo buildah images
