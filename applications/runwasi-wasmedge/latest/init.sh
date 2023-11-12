3803c494dd6684012f656a93131fa1aaf3629e47
cuisongliu <cuisongliu@qq.com>
2023-11-12 19:33:39 +0800

    feature(main): wasm
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- applications/runwasi-wasmedge/latest/init.sh
+++ applications/runwasi-wasmedge/latest/init.sh
@@ -12,8 +12,7 @@ cat <<EOF >>"Kubefile"
 FROM scratch
 COPY --from=ghcr.io/second-state/runwasi-wasmedge-plugin:allinone.${VERSION} . ${NAME}
 LABEL sealos.io.type="patch"
-COPY install.sh install.sh
-COPY containerd.sh containerd.sh
-COPY runtime.yaml runtime.yaml
-CMD ["bash install.sh"]
+COPY install.sh ${NAME}/install.sh
+COPY runtime.yaml ${NAME}/runtime.yaml
+CMD ["bash ${NAME}/install.sh","kubectl apply -f ${NAME}/runtime.yaml"]
 EOF
--- applications/runwasi-wasmedge/latest/install.sh
+++ applications/runwasi-wasmedge/latest/install.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
-cp -rvf runwasi-wasmedge/ /opt/containerd/
-bash containerd.sh
-kubectl apply -f runtime.yaml
+cp -rvf bin/ /opt/containerd/bin/
+cp -rvf lib/ /opt/containerd/lib/
+
+CONFIG_FILE="/etc/containerd/config.toml"
+BACKUP_FILE="/etc/containerd/config.toml.bak"
+
+# 备份原始配置文件
+sudo cp "$CONFIG_FILE" "$BACKUP_FILE"
+
+# 添加 WasmEdge 运行时配置
+if ! grep -q "io.containerd.wasmedge.v1" "$CONFIG_FILE"; then
+    sudo sed -i '/\[plugins."io.containerd.grpc.v1.cri".containerd.runtimes\]/a \ \ \ \ \ \ \ \ \[plugins."io.containerd.grpc.v1.cri".containerd.runtimes.wasmedge]\n          runtime_type = "io.containerd.wasmedge.v1"' "$CONFIG_FILE"
+    echo "WasmEdge runtime configuration added."
+    sudo systemctl restart containerd
+    echo "Containerd restarted."
+else
+    echo "WasmEdge runtime configuration already exists."
+fi
+
