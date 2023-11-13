5f995c9cde46d474997568a46770670e7ca79b7a
cuisongliu <cuisongliu@qq.com>
2023-11-13 12:53:05 +0800

    feature(main): wasm
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- applications/containerd-shim-wasmtime/latest/init.sh
+++ applications/containerd-shim-wasmtime/latest/init.sh
@@ -8,11 +8,18 @@ export readonly ARCH=${1:-amd64}
 export readonly NAME=${2:-$(basename "${PWD%/*}")}
 export readonly VERSION=${3:-latest}
 
+if [ "$ARCH" == "arm64" ]; then
+    ARCH="aarch64"
+fi
+mkdir -p ${NAME}/bin
+wget -O runwasi.tar.gz https://github.com/containerd/runwasi/releases/download/${NAME}/${VERSION}/${NAME}-${ARCH}.tar.gz
+tar -zxvf runwasi.tar.gz -C ${NAME}/bin
+
+
 cat <<EOF >>"Kubefile"
 FROM scratch
+COPY ${NAME} ${NAME}
 LABEL sealos.io.type="patch"
-COPY install.sh install.sh
-COPY containerd.sh containerd.sh
-COPY runtime.yaml runtime.yaml
-CMD ["bash install.sh"]
+COPY install.sh ${NAME}/install.sh
+CMD ["bash ${NAME}/install.sh ${NAME}"]
 EOF
--- applications/containerd-shim-wasmtime/latest/install.sh
+++ applications/containerd-shim-wasmtime/latest/install.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
-cp -rvf bin /opt/containerd/
-bash containerd.sh
-kubectl apply -f runtime.yaml
+cp -rvf "${1}"/bin/* /opt/containerd/bin/
+
+# shellcheck disable=SC1035
+if [[-s "${1}"/lib ]]; then
+    cp -rvf "${1}"/lib/* /opt/containerd/lib/
+fi
+
+CONFIG_FILE="/etc/containerd/config.toml"
+BACKUP_FILE="/etc/containerd/config.toml.bak"
+
+# 备份原始配置文件
+sudo cp "$CONFIG_FILE" "$BACKUP_FILE"
+
+# 添加 WasmRuntime 运行时配置
+if ! grep -q "io.containerd.wasmedge.v1" "$CONFIG_FILE"; then
+    sudo sed -i '/\[plugins."io.containerd.grpc.v1.cri".containerd.runtimes\]/a \ \ \ \ \ \ \ \ \[plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runwasi-wasmtime]\n          runtime_type = "io.containerd.wasmtime.v1"' "$CONFIG_FILE"
+    echo "WasmRuntime runtime configuration added."
+    sudo systemctl restart containerd
+    echo "Containerd restarted."
+else
+    echo "WasmRuntime runtime configuration already exists."
+fi
+
+kubectl apply -f - <<EOF
+apiVersion: node.k8s.io/v1
+kind: RuntimeClass
+metadata:
+  name: runwasi-wasmtime
+handler: runwasi-wasmtime
+EOF
