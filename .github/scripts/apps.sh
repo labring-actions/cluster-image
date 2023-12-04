da25f1d5d3f806b6995c2f7de2687fad8b3bac86
huangzhangshu <huangzhangshu@apecloud.com>
2023-12-05 01:17:41 +0800

    chore: test CLOUD_VALUES

--- .github/scripts/apps.sh
+++ .github/scripts/apps.sh
@@ -42,7 +42,7 @@ cd $buildDir && {
     sudo sealos login -u "$IMAGE_HUB_USERNAME" -p "$IMAGE_HUB_PASSWORD" "$IMAGE_HUB_REGISTRY"
 
     IMAGE_BUILD="${IMAGE_NAME%%:*}:build-$(date +%s)"
-    sudo sealos build -t "$IMAGE_BUILD" --max-pull-procs=20 --isolation=chroot --platform "linux/$APP_ARCH" --creds "$IMAGE_HUB_USERNAME:$IMAGE_HUB_PASSWORD" -f $Kubefile .
+    sudo sealos build -t "$IMAGE_BUILD" --max-pull-procs=20 --isolation=chroot --platform "linux/$APP_ARCH" -f $Kubefile .
 
     echo "init $IMAGE_NAME success"
 }
