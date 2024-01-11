95d30dcb8e0683fa22d2261b3d0e4b9d905da1f7
huangzhangshu <huangzhangshu@apecloud.com>
2024-01-11 12:18:14 +0800

    chore: test ls charts

--- .github/scripts/apps.sh
+++ .github/scripts/apps.sh
@@ -44,8 +44,12 @@ cd $buildDir && {
     sudo sealos login -u "$IMAGE_HUB_USERNAME" -p "$IMAGE_HUB_PASSWORD" "$IMAGE_HUB_REGISTRY"
 
     IMAGE_BUILD="${IMAGE_NAME%%:*}:build-$(date +%s)"
-    ls $buildDir/charts
-    ls $buildDir/charts/*
+    ehco "ls *"
+    ls *
+    ehco "ls charts"
+    ls charts
+    ehco "ls charts/*"
+    ls charts/*
     sudo sealos build -t "$IMAGE_BUILD" --max-pull-procs=20 --isolation=chroot --platform "linux/$APP_ARCH" -f $Kubefile .
 
     echo "init $IMAGE_NAME success"
