01d9bb39f7b51e96db807f5b5fdbfb32d887bf86
huangzhangshu <huangzhangshu@apecloud.com>
2024-01-25 10:43:09 +0800

    chore: add kubebench images

--- applications/kubebench/latest/init.sh
+++ applications/kubebench/latest/init.sh
@@ -15,3 +15,13 @@ for chart in "${charts[@]}"; do
     helm fetch -d charts --untar "$repo_url"/"${chart}"-"${VERSION#v}"/"${chart}"-"${VERSION#v}".tgz
     rm -rf charts/"${chart}"-"${VERSION#v}".tgz
 done
+
+# add extra images
+mkdir -p images/shim
+echo "registry.cn-hangzhou.aliyuncs.com/apecloud/spilo:14.8.0" >images/shim/kubebenchImages
+echo "registry.cn-hangzhou.aliyuncs.com/apecloud/customsuites:latest" >>images/shim/kubebenchImages
+echo "registry.cn-hangzhou.aliyuncs.com/apecloud/benchmarksql:latest" >>images/shim/kubebenchImages
+echo "registry.cn-hangzhou.aliyuncs.com/apecloud/go-ycsb:latest" >>images/shim/kubebenchImages
+echo "registry.cn-hangzhou.aliyuncs.com/apecloud/fio:latest" >>images/shim/kubebenchImages
+echo "registry.cn-hangzhou.aliyuncs.com/apecloud/redis:7.0.5" >>images/shim/kubebenchImages
+echo "registry.cn-hangzhou.aliyuncs.com/apecloud/kubebench:0.0.6" >>images/shim/kubebenchImages
