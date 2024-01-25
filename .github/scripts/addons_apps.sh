cb9d51f89cbc4067443305ae628ef91b82505d12
huangzhangshu <huangzhangshu@apecloud.com>
2024-01-25 12:08:24 +0800

    chore: add addon build workflow release

---
+++ .github/scripts/addons_apps.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -eu
+
+readonly APP_NAME=${app?}
+readonly APP_VERSION=${version?}
+readonly APP_ARCH=${arch?}
+
+readonly IMAGE_HUB_REGISTRY=${registry?}
+readonly IMAGE_HUB_REPO=${repo?}
+readonly IMAGE_HUB_USERNAME=${username?}
+readonly IMAGE_HUB_PASSWORD=${password?}
+
+readonly buildDir=.build-image
+
+rm -rf $buildDir
+mkdir -p $buildDir
+
+if [[ ! -d "applications/$APP_NAME/latest" ]]; then
+    sed -i "s/^CMD.*/CMD [\"kbcli addon enable $APP_NAME\"]/" applications/addons/latest/Dockerfile
+    sed -i "s/^charts=.*/charts=(\"$APP_NAME\")/" applications/addons/latest/init.sh
+    cp -af applications/addons applications/$APP_NAME
+fi
+
+if [[ -d "applications/$APP_NAME/latest" ]] && ! [[ -d "applications/$APP_NAME/$APP_VERSION" ]]; then
+    cp -af .github/scripts/apps/ /tmp/scripts_apps
+    cp -af "applications/$APP_NAME/latest" "applications/$APP_NAME/$APP_VERSION"
+fi
+
+cp -rf "applications/$APP_NAME/$APP_VERSION"/* $buildDir
+
+cd $buildDir && {
+    [[ -s Dockerfile ]] && Kubefile="Dockerfile" || Kubefile="Kubefile"
+
+    if [[ -s "build_arch" ]]; then
+        FILE_CONTENT=$(cat "build_arch"| tr -d '[:space:]'| tr -d '\n'| tr -d '\t')
+        if [[ "$FILE_CONTENT" != "$APP_ARCH" ]]; then
+            echo "The content of build_arch does not match the ARCH variable. Exiting."
+            exit 0
+        fi
+    fi
+    if [[ -s init.sh ]]; then
+        bash init.sh "$APP_ARCH" "$APP_NAME" "$APP_VERSION"
+    fi
+
+    IMAGE_NAME="$IMAGE_HUB_REGISTRY/$IMAGE_HUB_REPO/$APP_NAME-airgap:$APP_VERSION-$APP_ARCH"
+
+    sudo sealos login -u "$IMAGE_HUB_USERNAME" -p "$IMAGE_HUB_PASSWORD" "$IMAGE_HUB_REGISTRY"
+
+    IMAGE_BUILD="${IMAGE_NAME%%:*}:build-$(date +%s)"
+
+    sudo sealos build -t "$IMAGE_BUILD" --max-pull-procs=20 --isolation=chroot --platform "linux/$APP_ARCH" -f $Kubefile .
+
+    sudo sealos tag "$IMAGE_BUILD" "$IMAGE_NAME" && sudo sealos rmi -f "$IMAGE_BUILD"
+
+    sudo sealos login -u "$IMAGE_HUB_USERNAME" -p "$IMAGE_HUB_PASSWORD" "$IMAGE_HUB_REGISTRY" &&
+    sudo sealos push "$IMAGE_NAME" && echo "$IMAGE_NAME push success"
+}
+
+sudo buildah images
--- applications/kubebench/latest/init.sh
+++ applications/kubebench/latest/init.sh
@@ -18,10 +18,10 @@ done
 
 # add extra images
 mkdir -p images/shim
-echo "registry.cn-hangzhou.aliyuncs.com/apecloud/spilo:14.8.0" >images/shim/kubebenchImages
-echo "registry.cn-hangzhou.aliyuncs.com/apecloud/customsuites:latest" >>images/shim/kubebenchImages
+echo "registry.cn-hangzhou.aliyuncs.com/apecloud/customsuites:latest" >images/shim/kubebenchImages
 echo "registry.cn-hangzhou.aliyuncs.com/apecloud/benchmarksql:latest" >>images/shim/kubebenchImages
-echo "registry.cn-hangzhou.aliyuncs.com/apecloud/go-ycsb:latest" >>images/shim/kubebenchImages
-echo "registry.cn-hangzhou.aliyuncs.com/apecloud/fio:latest" >>images/shim/kubebenchImages
-echo "registry.cn-hangzhou.aliyuncs.com/apecloud/redis:7.0.5" >>images/shim/kubebenchImages
-echo "registry.cn-hangzhou.aliyuncs.com/apecloud/kubebench:0.0.6" >>images/shim/kubebenchImages
+#echo "registry.cn-hangzhou.aliyuncs.com/apecloud/spilo:14.8.0" >>images/shim/kubebenchImages
+#echo "registry.cn-hangzhou.aliyuncs.com/apecloud/go-ycsb:latest" >>images/shim/kubebenchImages
+#echo "registry.cn-hangzhou.aliyuncs.com/apecloud/fio:latest" >>images/shim/kubebenchImages
+#echo "registry.cn-hangzhou.aliyuncs.com/apecloud/redis:7.0.5" >>images/shim/kubebenchImages
+#echo "registry.cn-hangzhou.aliyuncs.com/apecloud/kubebench:0.0.6" >>images/shim/kubebenchImages
