e9b6eaafba59c9443f53e2509815e39cced133ed
huangzhangshu <huangzhangshu@apecloud.com>
2024-03-01 13:10:09 +0800

    chore: change cilium sidecarImageRegex version

--- applications/cilium/latest/init.sh
+++ applications/cilium/latest/init.sh
@@ -13,6 +13,6 @@ repo_url="https://github.com/apecloud/helm-charts/releases/download"
 charts=("cilium")
 for chart in "${charts[@]}"; do
     helm fetch -d charts --untar "$repo_url"/"${chart}"-"${VERSION#v}"/"${chart}"-"${VERSION#v}".tgz
-    yq e -i '.proxy.sidecarImageRegex=cilium/istio_proxy:1.10.3' charts/${chart}/values.yaml
+    yq e -i '.proxy.sidecarImageRegex="cilium/istio_proxy:1.10.3"' charts/${chart}/values.yaml
     rm -rf charts/"${chart}"-"${VERSION#v}".tgz
 done
