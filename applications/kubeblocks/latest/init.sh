92233eac48e7b0301a19d6a6b8dda953f871bfba
huangzhangshu <huangzhangshu@apecloud.com>
2024-03-27 20:13:57 +0800

    chore: install kubeblocks crds before deploy kubeblocks

--- applications/kubeblocks/latest/init.sh
+++ applications/kubeblocks/latest/init.sh
@@ -24,6 +24,15 @@ charts=("kubeblocks")
 for chart in "${charts[@]}"; do
   chart_version=${VERSION#v}
   helm fetch -d charts --untar "$repo_url"/"${chart}"-"${chart_version}"/"${chart}"-"${chart_version}".tgz
+  if [[ "$chart" == "kubeblocks" && "$VERSION" != "v0.5."* && "$VERSION" != "v0.6."*  && "$VERSION" != "v0.7."*  ]]; then
+    kubeblocks_crds_file="kubeblocks_crds.yaml"
+    wget https://github.com/apecloud/kubeblocks/releases/download/${VERSION}/${kubeblocks_crds_file} -O ${kubeblocks_crds_file}
+    kubeblocks_crds_dir="charts/${chart}/crds"
+    mkdir -p ${kubeblocks_crds_dir}
+    if [[ -f "${kubeblocks_crds_file}" ]]; then
+      mv ${kubeblocks_crds_file} ${kubeblocks_crds_dir}
+    fi
+  fi
   rm -rf charts/"${chart}"-"${chart_version}".tgz
 done
 
