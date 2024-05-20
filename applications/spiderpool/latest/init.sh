8df8160bbcec9b3ce11b09f4244064e3633b8908
huangzhangshu <huangzhangshu@apecloud.com>
2024-05-20 10:13:24 +0800

    chore: change sipderpool set values

--- applications/spiderpool/latest/init.sh
+++ applications/spiderpool/latest/init.sh
@@ -13,5 +13,13 @@ repo_url="https://github.com/apecloud/helm-charts/releases/download"
 charts=("spiderpool")
 for chart in "${charts[@]}"; do
     helm fetch -d charts --untar "$repo_url"/"${chart}"-"${VERSION#v}"/"${chart}"-"${VERSION#v}".tgz
+    values_file="charts/${charts}/values.yaml"
+    yq e -i '.multus.multusCNI.install=false' $values_file
+    yq e -i '.spiderpoolAgent.image.registry=docker.io' $values_file
+    yq e -i '.spiderpoolAgent.image.repository=apecloud/spiderpool-agent' $values_file
+    yq e -i '.spiderpoolAgent.image.tag=eb82b45a097ad23906e72199b70e25d161fbe88d' $values_file
+    yq e -i '.spiderpoolController.image.registry=docker.io' $values_file
+    yq e -i '.spiderpoolController.image.repository=apecloud/spiderpool-controller' $values_file
+    yq e -i '.spiderpoolController.image.tag=eb82b45a097ad23906e72199b70e25d161fbe88d' $values_file
     rm -rf charts/"${chart}"-"${VERSION#v}".tgz
 done
