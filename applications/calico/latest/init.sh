9180dde5ce5a525e91f02827d8d93704edcf0952
Guo Ziang <ziang.gza@apecloud.com>
2024-05-28 23:13:47 +0800

    feat: support calico

---
+++ applications/calico/latest/init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ex
+
+cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
+export readonly ARCH=${1:-amd64}
+export readonly NAME=${2:-$(basename "${PWD%/*}")}
+export readonly VERSION=${3:-$(basename "$PWD")}
