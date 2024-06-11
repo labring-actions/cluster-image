8433115984e71c712c3278738066f5a19e829c77
jiahui <bxy4543@gmail.com>
2024-06-11 16:32:19 +0800

    fix

--- applications/openebs-lvm/latest/init.sh
+++ applications/openebs-lvm/latest/init.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+set -e
+cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
+export readonly VERSION=${3:-$(basename "$PWD")}
 
 mkdir -p manifests
 wget "https://raw.githubusercontent.com/openebs/lvm-localpv/v${VERSION#*v}/deploy/lvm-operator.yaml" -O manifests/lvm-operator.yaml
