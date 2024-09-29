a574d16c4cff61cee3ac8c3bce77f23feae1bbb3
huangzhangshu <huangzhangshu@apecloud.com>
2024-09-29 22:35:13 +0800

    chore: fix manifests_charts_image_check error

--- .github/scripts/manifests_charts_image_check.sh
+++ .github/scripts/manifests_charts_image_check.sh
@@ -122,6 +122,7 @@ check_charts_images() {
                 set_values="${set_values} --set images.relay.tag=${chart_version} "
                 set_values="${set_values} --set images.cr4w.tag=${chart_version} "
                 set_values="${set_values} --set images.openconsole.tag=${chart_version} "
+                set_values="${set_values} --set images.openconsoleAdmin.tag=${chart_version} "
                 set_values="${set_values} --set images.taskManager.tag=${chart_version} "
             ;;
             kb-cloud-installer)
